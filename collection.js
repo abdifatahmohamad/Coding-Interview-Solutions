5fe120567a9a95588e668f45cf89dfd72a054b66
Abdifatah Ali <48977326+abdifatahali@users.noreply.github.com>
2020-05-14 02:34:23 -0500

    Update collection.js

--- collection.js
+++ collection.js
@@ -20,26 +20,15 @@ let fizzBuzz = function (n) {
 	return numbers;
 };
 
-// Find ages above 21 using FILTER method
-const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
-onst canDrink = ages.filter(age => age >= 21);
+/*Write a function getLastElement that takes an array and returns the last element of the array. 
+getLastElement([1, 2]) should return 2. */
 
-console.log(canDrink);
+function getLastElement(arr){
+   for(let i = 0;  i < arr.length; i++){
+   return arr[arr.length-1];
+  }
+}
 
-//Get all the companies that started in 80s using FILTER method
-const companies= [
-  {name: "Company One", category: "Finance", start: 1981, end: 2004},
-  {name: "Company Two", category: "Retail", start: 1992, end: 2008},
-  {name: "Company Three", category: "Auto", start: 1999, end: 2007},
-  {name: "Company Four", category: "Retail", start: 1989, end: 2010},
-  {name: "Company Five", category: "Technology", start: 2009, end: 2014},
-  {name: "Company Six", category: "Finance", start: 1987, end: 2010},
-  {name: "Company Seven", category: "Auto", start: 1986, end: 1996},
-  {name: "Company Eight", category: "Technology", start: 2011, end: 2016},
-  {name: "Company Nine", category: "Retail", start: 1981, end: 1989}
-];
-
-const eightiesCompanies = companies.filter(company => company.start >= 1980 && company.start < 1990);
-console.log(eightiesCompanies);
+ console.log( getLastElement([1,2])); //should return 2
 
 
