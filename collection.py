8caf9b3191416db229fb2c3734fafbf8b8d4ebec
Abdifatah Ali <48977326+abdifatahali@users.noreply.github.com>
2020-06-07 00:43:30 -0500

    Update collection.py

--- collection.py
+++ collection.py
@@ -1,1 +1,36 @@
 //Two sum number
+
+
+// Valid Anagram
+
+def valid_anagram(first, second):
+	if len(first) != len(second):
+		return False
+		
+	
+
+
+	lookup = {}
+	
+	for char in first:
+		letter  = first[char]
+		if lookup[letter]:
+			lookup[letter] += 1
+		else:
+			lookup[letter] = 1
+			
+		
+	for char in second:
+		letter = second[char]
+		if not lookup[letter]:
+			return False
+		else:
+			lookup[letter] -= 1
+	
+	 	
+	
+    return True
+	 
+	
+
+print(valid_anagram("anagram", "nagaram"))
