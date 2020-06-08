477fe71476f28e0a3f5519896a7064c5bfec4201
Abdifatah Ali <48977326+abdifatahali@users.noreply.github.com>
2020-06-07 22:25:54 -0500

    Update collection.py

--- collection.py
+++ collection.py
@@ -1,36 +1,24 @@
-//Two sum number
-
-
 // Valid Anagram
 
-def valid_anagram(first, second):
-	if len(first) != len(second):
-		return False
-		
-	
+import re
 
+def valid_anagram(str1, str2):
+   # Remove special character to the string:
+    str1 = re.sub('[^A-Za-z0-9]+', '', str1).lower()
+    str2 = re.sub('[^A-Za-z0-9]+', '', str2).lower()
+   # Cast string as a list
+    list_str1, list_str2 = list(str1), list(str2)
+   # Sort the string	  
+    list_str1.sort()
+    list_str2.sort()
+   # Compare the two string and return a boolean	
+    return (list_str1 == list_str2)   
 
-	lookup = {}
-	
-	for char in first:
-		letter  = first[char]
-		if lookup[letter]:
-			lookup[letter] += 1
-		else:
-			lookup[letter] = 1
-			
-		
-	for char in second:
-		letter = second[char]
-		if not lookup[letter]:
-			return False
-		else:
-			lookup[letter] -= 1
-	
-	 	
-	
-    return True
-	 
-	
-
-print(valid_anagram("anagram", "nagaram"))
+print(valid_anagram('', '')) # True
+print(valid_anagram('', '?')) # True
+print(valid_anagram('aaz', 'zza')) # False
+print(valid_anagram('Anagram', 'nagaram')) # True
+print(valid_anagram('rat', 'car')) # False
+print(valid_anagram('awesome', 'awesom')) # False
+print(valid_anagram('qwerty', 'qeywrt')) # True
+print(valid_anagram('texttwisttime', 'timetwisttext')) # Frue
