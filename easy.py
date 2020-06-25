5c7d84a3d16bbeea97c3533c1553bc64ad2b1fc9
Abdifatah Mohamed <48977326+abdifatahdev@users.noreply.github.com>
2020-06-25 18:25:32 -0500

    Update easy.py

--- easy.py
+++ easy.py
@@ -236,7 +236,7 @@ print(paper_doll('Hello'))'''
 print(blackjack(9, 9, 9))
 # print(blackjack(9,9,11))'''
 
-##############################################################################################
+###############################################################################################
 # SUMMER OF '69: Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6
 # and extending to the next 9 (every 6 will be followed by at least one 9). Return 0 for no numbers.
 # summer_69([1, 3, 5]) --> 9
