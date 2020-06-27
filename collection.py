a88c602f65e320c740ae2d911d3ad3409650bfac
Abdifatah Mohamed <48977326+abdifatahdev@users.noreply.github.com>
2020-06-26 20:15:06 -0500

    Update collection.py

--- collection.py
+++ collection.py
@@ -289,5 +289,30 @@ solution = Solution()
 print(solution.isPalindrome(s))
 
 #################################################################################################
+# Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+# Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+# Input: nums = [1,2,3,4,4,3,2,1], n = 4
+# Output: [1,4,2,3,3,2,4,1]
+
+from typing import List
+
+
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        result = []
+        for i in range(n):
+            result.append(nums[i])
+            result.append(nums[n + i])
+
+        return result
+
+
+nums = [2, 5, 1, 3, 4, 7]
+n = 3
+solution = Solution()
+print(solution.shuffle(nums, n))
+
+##################################################################################################
+
 
 
