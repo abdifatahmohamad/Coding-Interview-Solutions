12bab42bafc278ac983ba870ffb029d4253fff79
Abdifatah Mohamed <48977326+abdifatahdev@users.noreply.github.com>
2020-07-11 18:43:24 -0500

    Update collection.py

--- collection.py
+++ collection.py
@@ -191,7 +191,7 @@ def mergeSortedArray(arr1, arr2):
 
 print(mergeSortedArray([0, 3, 4, 31], [4, 6, 30])) # Output: [0, 3, 4, 4, 6, 30, 31]
 
-#################################################################################################
+##################################################################################################
 # Given an array with n objects colored red, white or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white and blue.
 # Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
 # Note: You are not suppose to use the library's sort function for this problem.
