617449d2d270321b0a7e33881089207fb338b51d
Abdifatah Mohamed <48977326+abdifatahdev@users.noreply.github.com>
2020-07-16 10:39:11 -0500

    Update collection.py

--- collection.py
+++ collection.py
@@ -6,12 +6,12 @@ def valid_anagram(str1, str2):
    # Remove special character to the string:
     str1 = re.sub('[^A-Za-z0-9]+', '', str1).lower()
     str2 = re.sub('[^A-Za-z0-9]+', '', str2).lower()
-   # Cast string as a list
+   # Cast string as a list:
     list_str1, list_str2 = list(str1), list(str2)
    # Sort the string	  
     list_str1.sort()
     list_str2.sort()
-   # Compare the two string and return a boolean	
+   # Compare the two string and return a boolean:	
     return (list_str1 == list_str2)   
 
 print(valid_anagram('', '')) # True
