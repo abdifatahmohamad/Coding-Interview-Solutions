24a1447a5f04581d7c342efb94598bfdb99960d1
Abdifatah Mohamed <48977326+abdifatahdev@users.noreply.github.com>
2020-07-16 19:24:41 -0500

    Update easy.py

--- easy.py
+++ easy.py
@@ -399,7 +399,7 @@ print(remove_duplicates(arr))
 # Remove the left-most duplicates from a list of integers and return the result.
 # Remove the 3's at indices 0 and 3 followed by removing a 4 at index 1
 # Input: [3, 4, 4, 3, 6, 3] # => [4, 6, 3], Input: [1,2,1,2,1,2,3] Output:[1,2,3]
-def solve(arr):
+def remove_duplicates(arr):
     dupes = []
     array = arr[::-1]
     for num in array:
@@ -420,7 +420,7 @@ def remove_duplicates(arr):
     return result
 
 # Using list comprehension:
-def solve(arr):
+def remove_duplicates(arr):
     result = [array for index, array in enumerate(arr) if array not in arr[index + 1:]]
     return result
 
