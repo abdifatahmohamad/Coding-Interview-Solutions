64dffe552d8ee18a1c3769a46cc0e8ed8857d515
Abdifatah Mohamed <48977326+abdifatahdev@users.noreply.github.com>
2020-07-17 22:18:42 -0500

    Update collection.py

--- collection.py
+++ collection.py
@@ -41,7 +41,6 @@ def ispangram(str1, alphabet=string.ascii_lowercase):
     alphaset = set(alphabet)
     return alphaset <= set(str1)
 
-
 print(ispangram('The quick brown fox jumps over the lazy dog'))  # output: True
 print(ispangram("This string is missing some letters"))  # output: False
 ####################################################################################################
