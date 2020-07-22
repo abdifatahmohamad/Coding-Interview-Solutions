ae51537a25aa4946bfb2fae60ac4921a3d5c768a
Abdifatah Mohamed <48977326+abdifatahdev@users.noreply.github.com>
2020-07-22 00:02:24 -0500

    Update easy.py

--- easy.py
+++ easy.py
@@ -48,7 +48,7 @@ for word in st.split():
   if len(word) % 2 == 0:
     print(f'EVEN strings are: {word}')'''
 
-###############################################################################
+################################################################################
 #  Use List Comprehension to create a list of the first letters of every word in the string below:
 '''st = 'Create a list of the first letters of every word in this string'
 my_list = [word[0] for word in st.split()]
