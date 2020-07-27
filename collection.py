f91291a2210f10bafe9343906b3a49dbc9342586
Abdifatah Mohamed <48977326+abdifatahdev@users.noreply.github.com>
2020-07-27 16:38:40 -0500

    Update collection.py

--- collection.py
+++ collection.py
@@ -6,22 +6,33 @@ def valid_anagram(str1, str2):
     str1 = re.sub('[^A-Za-z0-9]+', '', str1).lower()
     str2 = re.sub('[^A-Za-z0-9]+', '', str2).lower()
    # Cast string as a list:
-    list_str1, list_str2 = list(str1), list(str2)
-   # Sort the string	  
+    list_str1, list_str2 = list(str1), list(str2)  
     list_str1.sort()
-    list_str2.sort()
-   # Compare the two string and return a boolean:	
-    return (list_str1 == list_str2)   
+    list_str2.sort()	
+    return (list_str1 == list_str2)
 
-print(valid_anagram('', '')) # True
-print(valid_anagram('', '?')) # True
-print(valid_anagram('aaz', 'zza')) # False
+# Another way of doing it using a HashMap:
+def isAnagram(s: str, t: str) -> bool:
+    if len(s) != len(t):
+        return False
+    seen = {}
+    for char in s:
+        if char not in seen:
+            seen[char] = 1
+        else:
+            seen[char] += 1
+
+    for letter in t:
+        if letter not in seen:
+            return False
+
+        if seen[letter] < 1:
+            return False
+        else:
+            seen[letter] -= 1
+    return True
+
 print(valid_anagram('Anagram', 'nagaram')) # True
-print(valid_anagram('rat', 'car')) # False
-print(valid_anagram('awesome', 'awesom')) # False
-print(valid_anagram('qwerty', 'qeywrt')) # True
-print(valid_anagram('texttwisttime', 'timetwisttext')) # True
-
 #####################################################################################################
 # Write a Python function to check whether a string is pangram or not.
 # Note : Pangrams are words or sentences containing every letter of the alphabet at least once.
