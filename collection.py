ece75c2281bccbc2c6431798503dbb0f83f36ec8
Abdifatah Mohamed <48977326+abdifatahdev@users.noreply.github.com>
2020-08-01 19:06:48 -0500

    Update collection.py

--- collection.py
+++ collection.py
@@ -351,7 +351,7 @@ n = 3
 solution = Solution()
 print(solution.shuffle(nums, n))
 
-###################################################################################################
+##################################################################################################
 # The task is to find their comparison points by comparing a[0] with b[0], a[1] with b[1], and a[2] with b[2].
 # If a[i] > b[i], then Alice is awarded 1 point.
 # If a[i] , then Bob is awarded 1 point.
