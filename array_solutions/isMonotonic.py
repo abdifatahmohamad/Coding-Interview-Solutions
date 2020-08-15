dee1d3554113906a9429d5ea5925ff290769c57e
Abdifatah Mohamed <abdifatahmdev@gmail.com>
2020-08-15 00:47:17 -0500

    Added isMonotonic file

---
+++ array_solutions/isMonotonic.py
@@ -0,0 +1,16 @@
+# O(n) time | O(1) space
+def isMonotonic(array):
+    increasing = True
+    decreasing = True
+    for i in range(len(array) - 1):
+        if array[i] > array[i + 1]:
+            increasing = False
+        if array[i] < array[i + 1]:
+            decreasing = False
+
+    return increasing or decreasing
+
+
+array = [-1, -5, -10, -1100, -1100, -1101, -1102, -9001]
+# Output: True
+print(isMonotonic(array))
