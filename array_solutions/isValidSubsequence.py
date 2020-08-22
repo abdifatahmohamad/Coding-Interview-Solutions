72ce4bc3d771f71dd1be0b799e1d611baf97a545
Abdifatah Mohamed <mofaatah@gmail.com>
2020-08-21 22:14:10 -0500

    Added string solutions

--- array_solutions/isValidSubsequence.py
+++ array_solutions/isValidSubsequence.py
@@ -1,1 +1,22 @@
 # O(n) time | O(1) space --> where n is the length of the array
+def isValidSubsequence(array, sequence):
+	count = index = 0
+    for curr in array:
+        if count != len(sequence) and curr == sequence[index]:
+            count += 1
+            index += 1
+    return count == len(sequence)
+
+# Another way to solve it using for loop:
+def isValidSubsequence(array, sequence):
+	arrIdx = 0
+    seqIdx = 0
+    while (arrIdx < len(array)) and (seqIdx < len(sequence)):
+        if array[arrIdx] == sequence[seqIdx]:
+            seqIdx += 1
+        arrIdx += 1
+    return seqIdx == len(sequence)
+
+array = [5, 1, 22, 25, 6, -1, 8, 10]
+sequence = [1, 6, -1, 10]
+print(isValidSubsequence(array, sequence))
---
+++ string_solutions/reverse_words_in_a_stringIII.py
@@ -0,0 +1,21 @@
+# O(n) time | O(1) space
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        s = s.split()
+        for i, word in enumerate(s):
+            s[i] = self.helper(word)
+        return " ".join(s)
+
+    @staticmethod
+    def helper(word):
+        word = list(word)
+        left, right = 0, len(word) - 1
+        while left < right:
+            word[left], word[right] = word[right], word[left]
+            left += 1
+            right -= 1
+        return "".join(word)
+
+
+solution = Solution()
+print(solution.reverseWords("Let's take LeetCode contest"))
