83f2c38c6d0b88bf10c961860d442a2de22dbf75
Abdifatah Mohamed <48977326+abdifatahdev@users.noreply.github.com>
2020-09-15 21:26:31 -0500

    Update higher-order-functions.js

--- practice/higher-order-functions.js
+++ practice/higher-order-functions.js
@@ -2,7 +2,7 @@
 forEach(), every(), some(), sort() and so on to solve a complex problems. */
 
 /* Using map() method, create a function that takes an array with numbers 
- and return an array with the elements multiplied by two..*/
+ and return an array with the elements multiplied by two.*/
 
 const getMultipliedArr = (arr) => arr.map((num) => num * 2);
 
