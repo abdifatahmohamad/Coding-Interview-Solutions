3cbff52b25976a67b44d08388272fb51fd991b42
Abdifatah Mohamed <48977326+abdifatahdev@users.noreply.github.com>
2020-11-06 01:23:31 -0600

    Update easy.py

--- practice/easy.py
+++ practice/easy.py
@@ -200,7 +200,7 @@ def myfunc(word):
 
 print(myfunc("abdifatah"))
 
-##############################################################################################
+################################################################################################
 # LESSER OF TWO EVENS: Write a function that returns the lesser of two given numbers if both numbers are even,
 # but returns the greater if one or both numbers are odd¶
 
