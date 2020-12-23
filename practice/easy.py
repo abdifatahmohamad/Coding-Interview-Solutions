da13b3f9256e6cdd354ddc45235151a3bda6001b
Abdifatah Mohamed <48977326+abdifatahdev@users.noreply.github.com>
2020-12-23 00:20:29 -0600

    Update easy.py

--- practice/easy.py
+++ practice/easy.py
@@ -45,7 +45,7 @@ while keep_looping:
     else:
         print('Your guess is too low')
 
-###############################################################################
+##############################################################################
 #  Use List Comprehension to create a list of the first letters of every word in the string below:
 '''st = 'Create a list of the first letters of every word in this string'
 my_list = [word[0] for word in st.split()]
