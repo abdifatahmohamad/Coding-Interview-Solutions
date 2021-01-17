6f2339aaeff1d69334786814452bf7dc46b57a80
Abdifatah Mohamed <48977326+abdifatahdev@users.noreply.github.com>
2021-01-18 00:15:54 +0200

    Update easy.py

--- practice/easy.py
+++ practice/easy.py
@@ -213,7 +213,7 @@ print(myfunc("abdifatah"))
 
 
 print(lesser_of_two_evens(13,43))'''
-#############################################################################################
+###############################################################################################
 # ANIMAL CRACKERS: Write a function takes a two-word string and returns True if both words begin with same letter¶
 # animal_crackers('Levelheaded Llama') --> True
 # animal_crackers('Crazy Kangaroo') --> False
