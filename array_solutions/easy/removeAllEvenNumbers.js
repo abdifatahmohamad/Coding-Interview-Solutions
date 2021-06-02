6ca15ed7884bb55501c530236b01d26349462d57
Abdifatah Mohamed <mofaatah@gmail.com>
2021-06-01 22:32:33 -0500

    Add new easy solution

---
+++ array_solutions/easy/removeAllEvenNumbers.js
@@ -0,0 +1,15 @@
+function removeAllEvenNumbers(arr){
+    let odds =[]
+    for (let num of arr){
+        if(num % 2 != 0){
+            odds.push(num)
+        }
+    }
+    return odds;
+}
+
+
+
+let arr = [1,2,4,5,10,6,3];
+// Output: [1,5,3]
+console.log(removeAllEvenNumbers(arr));
