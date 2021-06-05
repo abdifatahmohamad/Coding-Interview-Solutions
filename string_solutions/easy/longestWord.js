0cc58fe861cc0170ea3b26ea069ab2b2ef3f1255
Abdifatah Mohamed <mofaatah@gmail.com>
2021-06-05 13:42:35 -0500

    Added new easy string solution

---
+++ string_solutions/easy/longestWord.js
@@ -0,0 +1,17 @@
+function longestWord(str){
+    let strSplit = str.split(' ');
+    let longestWord = 0;
+    let curr_word = null;
+    
+    for(let i=0; i<strSplit.length; i++){
+        if(strSplit[i].length > longestWord){
+            longestWord = strSplit[i].length;
+            curr_word = strSplit[i];
+        }
+    }
+
+    return curr_word;
+}
+
+str = "Abdifatah Abdiweli Mohamed";
+console.log(longestWord(str));
