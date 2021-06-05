641a803a28f66f6bf6951b0c43e52a7b3e095ded
Abdifatah Mohamed <mofaatah@gmail.com>
2021-06-05 14:02:24 -0500

    Added new easy string solution

--- string_solutions/easy/longestWord.js
+++ string_solutions/easy/longestWord.js
@@ -1,3 +1,4 @@
+// One way to solve this problem is to use a regular for loop (best solution)
 function longestWord(str){
     let strSplit = str.split(' ');
     let longestWord = 0;
@@ -11,6 +12,32 @@ function longestWord(str){
     }
 
     return curr_word;
+}
+
+// Another way to solve is to use Array.prototype.sort():
+function findLongestWord(str){
+    let strSplit = str.split(' ');
+
+    const longestWord = strSplit.sort((a, b) =>{
+    return b.length - a.length;
+});
+
+    return longestWord[0];
+}
+
+// The third way to solve is to use Array.prototype.sort():
+function findLongestWord(str){
+    let strSplit = str.split(' ');
+
+    const longestWord = strSplit.reduce((curr_word, longest) =>{
+    if(curr_word.length >  longest.length){
+        return curr_word;
+    } else{
+        return longest
+    }
+});
+
+    return longestWord;
 }
 
 let str = "Abdifatah Abdiweli Mohamed";
