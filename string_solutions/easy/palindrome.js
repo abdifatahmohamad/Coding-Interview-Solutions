7a5a9dd677b76627758a8c842921b737210207bf
Abdifatah Mohamed <mofaatah@gmail.com>
2021-06-06 11:07:22 -0500

    Added string solution

--- string_solutions/easy/palindrome.js
+++ string_solutions/easy/palindrome.js
@@ -9,5 +9,20 @@ function palindrome(str){
     return revString === str;
 }
 
+// Another way to solve is to use two pointers:
+const palindrome = str =>{
+    let left = 0;
+    let right = str.length-1;
+
+    while(left < right){
+        if(str[left] !== str[right]){
+            return false;
+        }
+        left += 1;
+        right -= 1;
+    }
+    return true;
+}
+
 let str = "level";
 console.log(palindrome(str));
