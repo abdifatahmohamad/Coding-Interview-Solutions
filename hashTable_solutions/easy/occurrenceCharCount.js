d70274d8fa19c1ce2f7d6135f162b90f2d41e28b
Abdifatah Mohamed <mofaatah@gmail.com>
2021-06-08 13:20:44 -0500

    Added hashtable solution

---
+++ hashTable_solutions/easy/occurrenceCharCount.js
@@ -0,0 +1,31 @@
+// ex. 'aabbccddeee' count the occurrence of characters
+
+//SOLUTION 1-  using for...of loop
+function count(str){
+    // Turn string into an array
+    const arr = Array.from(str);
+    let seen = {};
+    for(let char of arr){
+        if (char in seen){
+            seen[char] ++;
+        } else{
+            seen[char] = 1
+        }
+    }
+    return seen
+}
+
+let str = 'aabbccddeee';
+console.log(count(str));
+
+// SOLUTION 2- using the Array.prototype.reduce 
+const count = Array.from('aabbccddeee').reduce((acc, char) => {
+    if (char in acc) {
+      acc[char]++;
+    } else {
+      acc[char] = 1;
+    }
+    return acc;
+  }, {});
+
+  console.log(count);
---
+++ hashTable_solutions/easy/tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+let str = 'aabbccddeee';
+// console.log(count(str));
