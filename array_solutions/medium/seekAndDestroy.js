d0ad67ad438d13072103848465d16de392ef307c
Abdifatah Mohamed <mofaatah@gmail.com>
2021-06-07 23:43:21 -0500

    Added array solution

--- array_solutions/medium/seekAndDestroy.js
+++ array_solutions/medium/seekAndDestroy.js
@@ -2,8 +2,29 @@
 // Return the leftover values in an array:
 // ex. seekAndDestroy([2,3,4,6,6, 'hello'], 2,6) ==> [3,4, 'hello']
 
-function seekAndDestroy(arr, target){
+// Solution 1 using arguments, indexOf, filter
+function seekAndDestroy(arr){
+    // Turn the whole arguments into an array:
+    const args = Array.from(arguments);
 
+    // Create a helper function that checks target numbers
+    // and returns true if NOT in the actual array:
+    function filterArr(arr){
+        // Return true if NOT in the array:
+        return args.indexOf(arr) === -1;
+    }
+
+    // filter() return only if something is true
+    // Pass filterArr() into filter method(high order function method)
+    return arr.filter(filterArr);
+}
+
+// Solution 2 using ...rest, filter & includes
+function seekAndDestroy(arr){
+    return arr.filter(val => !resizeTo.includes(val));
 }
 
 console.log(seekAndDestroy([2,3,4,6,6, 'hello'], 2,6));
+
+
+
