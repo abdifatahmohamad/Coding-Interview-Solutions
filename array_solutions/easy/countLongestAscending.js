6ca0b8c1a0e36f011f4eeef431a3e035f7bca998
Abdifatah Mohamed <mofaatah@gmail.com>
2021-06-10 14:19:37 -0500

    Added solution

--- array_solutions/easy/countLongestAscending.js
+++ array_solutions/easy/countLongestAscending.js
@@ -1,3 +1,4 @@
+// SOLUTION 1
 function countLongestAscending(nums){
     let longestAscending = 1, currentLongestLength = 1;
 
@@ -20,6 +21,30 @@ function countLongestAscending(nums){
     }
 
     return longestAscending;
+}
+
+
+// SOLUTION 2
+function countLongestAscending(nums){
+    if(nums.length === 0){
+        return 0;
+    }
+    let max = 0, count = 0;
+
+    // Loop through the nums from the 2nd element
+    for(let i=1; i<nums.length; i++){
+        // Check if the current element is greater than previous element
+        if(nums[i] > nums[i-1]){
+            count++
+        } else{
+            count = 0;
+        }
+        if(count > max){
+            max = count;
+        }
+        }
+
+    return max + 1;
 }
 
 console.log(countLongestAscending([-1, 3, 5, 2, 5, 3, 9, 4, 8, 10, 12, 6])); // Output: 4
