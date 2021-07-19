9f4911ff396263acd8e2d93b539bdb54290e6d3c
Abdifatah Mohamed <mofaatah@gmail.com>
2021-07-19 03:03:26 -0500

    Added a solution

--- array_solutions/easy/FindCommonNumbers.java
+++ array_solutions/easy/FindCommonNumbers.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashMap;
 
 public class FindCommonNumbers {
     public static void main(String[] args) {
@@ -7,24 +8,42 @@ public class FindCommonNumbers {
         int arr2[] = { 17, 35, 39, 40, 55, 58, 60 };
         // Output: [35, 40, 55]
 
-        FindCommonNumbers fcn = new FindCommonNumbers();
-        System.out.println(fcn.findCommonNumbers(arr1, arr2));
-
+        System.out.println(findCommonNumbers(arr1, arr2));
     }
 
-    public List<Integer> findCommonNumbers(int[] arr1, int[] arr2) {
-        List<Integer> commonNumbers = new ArrayList<Integer>();
+    // Brute force approach
+    public static List<Integer> findCommonNumbers(int[] arr1, int[] arr2) {
+        List<Integer> result = new ArrayList<Integer>();
         for (int i = 0; i < arr1.length; i++) {
             for (int j = 0; j < arr2.length; j++) {
-                if (arr1[i] == arr2[j]) {
-                    // Check if the list already contains the common numbers
-                    if (!commonNumbers.contains(arr1[i])) {
-                        // Add the common numbers into the list
-                        commonNumbers.add(arr1[i]);
-                    }
-                }
+                if (arr1[i] == arr2[j])
+                    result.add(arr1[i]);
             }
         }
-        return commonNumbers;
+        return result;
+    }
+
+    // HashMap O(N) Time || O(N) Space
+    public static List<Integer> findCommonNumbers(int[] arr1, int[] arr2) {
+
+        HashMap<Integer, Integer> map = new HashMap();
+        List<Integer> newList = new ArrayList<Integer>();
+
+        for (int num : arr1) {
+            /**
+             * if(map.containsKey(num)){ map.put(num, map.get(num) + 1); }else{ map.put(num,
+             * 1); }
+             */
+
+            // Same as above code but shorter way:
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        for (int el : arr2) {
+            if (map.containsKey(el)) {
+                newList.add(el);
+            }
+        }
+        return newList;
     }
 }
