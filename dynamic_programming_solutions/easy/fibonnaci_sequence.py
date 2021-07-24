58ecbef3a356527a5c59f72a7f9c1fb12562ecd6
Abdifatah Mohamed <mofaatah@gmail.com>
2021-07-24 00:19:02 -0500

    Added a solution

--- dynamic_programming_solutions/easy/fibonnaci_sequence.py
+++ dynamic_programming_solutions/easy/fibonnaci_sequence.py
@@ -18,3 +18,34 @@ N = 3
 print(solution.fib(N))
 
 ###############################################
+
+
+class Solution(object):
+    def fib(self):
+
+        while True:
+            num = int(
+                input("Please enter how many numbers you want in this series: "))
+            first = 0
+            second = 1
+            if num == 0 or num == 1:
+                print(f'Please enter a number that is more than {num}')
+            else:
+                for number in range(num):
+                    print(first, end=' ')
+                    # Swap/Shift numbers in the list:
+                    temp = first + second
+                    first = second
+                    second = temp + second
+
+            playAgain = str(
+                input("\nDo you want to enter another number of series? (y/n): "))
+            if playAgain == 'y':
+                continue
+            else:
+                print("Thank you, maybe later!")
+                break
+
+
+solution = Solution()
+solution.fib()
