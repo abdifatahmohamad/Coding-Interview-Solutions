11ab6e0c63dbd6129ac8a3fc118bbd421475f746
Abdifatah Mohamed <mofaatah@gmail.com>
2021-07-25 14:42:13 -0500

    Added a solution

--- array_solutions/medium/sortArrayWithoutSortFunc.py
+++ array_solutions/medium/sortArrayWithoutSortFunc.py
@@ -19,11 +19,43 @@ class Solution:
                 current += 1
         return nums
 
+    # Using helper function to swap the values
     @staticmethod
     def swap(nums, i, j):
         temp = nums[i]
         nums[i] = nums[j]
         nums[j] = temp
+
+###########################################################################
+
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> List[int]:
+        start, current, end = 0, 0, len(nums) - 1
+
+        while current <= end:
+            if nums[current] == 0:
+                # temp = nums[current]
+                # nums[current] = nums[start]
+                # nums[start] = temp
+
+                # Same as above swap technique but in one line
+                nums[current], nums[start] = nums[start], nums[current]
+
+                current += 1
+                start += 1
+            elif nums[current] == 2:
+                # temp = nums[current]
+                # nums[current] = nums[end]
+                # nums[end] = temp
+
+                # Same as above swap technique but in one line
+                nums[current], nums[end] = nums[end], nums[current]
+
+                end -= 1
+            else:
+                current += 1
+        return nums
 
 
 solution = Solution()
