448995400097071f74994eebdfe0d18df35cb521
Abdifatah Mohamed <mofaatah@gmail.com>
2021-07-26 22:38:09 -0500

    Added a solution

---
+++ string_solutions/easy/missing_char.py
@@ -0,0 +1,22 @@
+# Test Case 1:
+# Input: s = 'kitten'
+# Input n = 1
+# Output: 'Ktten'
+
+# Test Case 2:
+# Input: s = 'kitten'
+# Input n = 0
+# Output: 'itten'
+
+# Test Case 3:
+# Input: s = 'kitten'
+# Input n = 4
+# Output: 'Kittn'
+
+class Solution:
+    def missing_char(self, s: str, n: int) -> str:
+        pass
+
+
+solution = Solution()
+print(solution.missing_char('kitten', 1))
