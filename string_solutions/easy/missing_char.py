f16c415ee1bb27bdb13e389a7a1a937acb0c3963
Abdifatah Mohamed <mofaatah@gmail.com>
2021-07-27 13:56:15 -0500

    Added a solution

--- string_solutions/easy/missing_char.py
+++ string_solutions/easy/missing_char.py
@@ -14,8 +14,23 @@
 # Output: 'Kittn'
 
 class Solution:
-    def missing_char(self, s: str, n: int) -> str:
-        pass
+    def missing_char(self, word: str, index: int) -> str:
+        # Solution 1 without loop:
+        if index > len(word)-1:
+            return ""
+
+        new_list = list(word)
+        new_list.pop(index)  # O(N)
+        return "".join(new_list)  # O(N)
+
+        # Solution 2 using for loop:
+        res = []
+        for i in range(len(word)):
+            # If curr idx doesn't equal to target idx, don't put in the list
+            if i != index:
+                res.append(word[i])
+        # Return joined list as a string
+        return "".join(res)
 
 
 solution = Solution()
