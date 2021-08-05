9d12b1a0b4e7f38b3888960e0e59950bb69d5408
Abdifatah Mohamed <mofaatah@gmail.com>
2021-08-05 13:24:56 -0500

    Added solution

--- linkedList_solutions/insertNodeAtHead.py
+++ linkedList_solutions/insertNodeAtHead.py
@@ -16,6 +16,7 @@ def print_list(head: LinkedList) -> None:
         head = head.next
 
 
+# O(1) Time and Space
 def insertNodeAtHead(head, val) -> LinkedList:
     new_node = LinkedList(val)
     if head != None:
@@ -31,4 +32,75 @@ node = insertNodeAtHead(node, "MIA")
 
 print_list(node)
 
+
 # Solution 2: inserting nodes as an array and then converting into a linked List:
+class LinkedList:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+def print_list(head: LinkedList) -> None:
+    while head is not None:
+        print(head.val, end=" <- ")
+        if head.next is None:
+            print("None")
+            break
+        head = head.next
+
+
+# O(N) Time || O(1) Space
+def insertAtHead(arr: List[str]) -> LinkedList:
+    head = LinkedList(arr[0])
+    for i in arr[1:]:
+        new_node = LinkedList(i)
+        new_node.next = head
+        head = new_node
+    return head
+
+
+airports = ["MSP", "LAX", "ORD", "SDF", "BOS"]
+nodes = insertAtHead(airports)
+print_list(nodes)
+
+#####################################################################
+
+
+# The above solution but using helper func
+class LinkedList:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+def print_list(head: LinkedList) -> None:
+    while head is not None:
+        print(head.val, end=" <- ")
+        if head.next is None:
+            print("None")
+            break
+        head = head.next
+
+
+# O(N) Time || O(1) Space
+def insertAtHead(arr: List[str]) -> LinkedList:
+    # Assign the first element in the list to be the head
+    head = LinkedList(arr[0])
+
+    # Since the first element is the head we start at index 1
+    for node in arr[1:]:
+        head = helper_function(head, node)
+    return head
+
+
+def helper_function(head: LinkedList, node) -> LinkedList:
+    # we will insert all the nodes at the end of the linked list
+    new_node = LinkedList(node)
+    new_node.next = head
+    head = new_node
+    return head
+
+
+airports = ["MSP", "LAX", "ORD", "SDF", "BOS"]
+nodes = insertAtHead(airports)
+print_list(nodes)
--- linkedList_solutions/insertNodeAtTail.py
+++ linkedList_solutions/insertNodeAtTail.py
@@ -16,6 +16,7 @@ def print_list(head: LinkedList) -> None:
         head = head.next
 
 
+# O(N) Time || O(1) Space
 def insertNodeAtTail(head, val) -> LinkedList:
     new_node = LinkedList(val)
     if head is None:
@@ -37,8 +38,6 @@ print_list(node)
 
 
 # Solution 2: inserting nodes as an array and then converting into a linked List:
-
-
 class LinkedList:
     def __init__(self, val):
         self.val = val
@@ -54,6 +53,7 @@ def print_list(head: LinkedList) -> None:
         head = head.next
 
 
+# O(N) Time || O(1) Space
 def insertAtTail(arr: List[str]) -> LinkedList:
     head = LinkedList(arr)
     if head is None:
@@ -66,6 +66,48 @@ def insertAtTail(arr: List[str]) -> LinkedList:
             last_node = last_node.next
         last_node.next = LinkedList(i)
     return head
+
+
+airports = ["MSP", "LAX", "ORD", "SDF", "BOS"]
+nodes = insertAtTail(airports)
+print_list(nodes)
+
+#####################################################################
+
+
+# The above solution but using helper func
+class LinkedList:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+def print_list(head: LinkedList) -> None:
+    while head is not None:
+        print(head.val, end=" -> ")
+        if head.next is None:
+            print("None")
+            break
+        head = head.next
+
+
+# O(N^2) Time || O(1) Space
+def insertAtTail(arr: List[str]) -> LinkedList:
+    # Assign the first element in the list to be the head
+    head = LinkedList(arr[0])
+
+    # Since the first element is the head we start at index 1
+    for node in arr[1:]:
+        helper_function(head, node)
+    return head
+
+
+def helper_function(head: LinkedList, node) -> None:
+    # we will insert all the nodes at the end of the linked list
+    last_node = head
+    while last_node.next:
+        last_node = last_node.next
+    last_node.next = LinkedList(node)
 
 
 airports = ["MSP", "LAX", "ORD", "SDF", "BOS"]
