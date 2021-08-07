c00b1ab7a1c74f465e7e92bc48c33663558b377b
Abdifatah Mohamed <mofaatah@gmail.com>
2021-08-06 23:37:58 -0500

    Added solution

--- array_solutions/easy/findIndexFromList.py
+++ array_solutions/easy/findIndexFromList.py
@@ -16,15 +16,35 @@ targetIdx = 3
 
 print(findIndexFromLast(arr, targetIdx))
 
+################################################
 
+
+def print_from_last(arr: List[str], pos) -> str:
+    if pos > len(arr) - 1:
+        return "Invalid Position."
+
+    k = 0
+    length = len(arr) - 1 - pos
+    while length > 0:
+        length -= 1
+        k += 1
+    return arr[k]
+
+
+arr = ["MSP", "BOS", "ATL", "LAX"]
+pos = 3
+
+print(print_from_last(arr, pos))
+
+################################################
 # O(1) Time and Space
-from typing import List
 
-def PFL(arr: List[str], pos)-> str:
-  if pos > len(arr) - 1:
-    return "Invalid Position"
-  return arr[len(arr) - 1 - pos]
-  
+
+def PFL(arr: List[str], pos) -> str:
+    if pos > len(arr) - 1:
+        return "Invalid Position"
+    return arr[len(arr) - 1 - pos]
+
 
 arr = ["MSP", "BOS", "ATL", "LAX"]
 pos = 3
