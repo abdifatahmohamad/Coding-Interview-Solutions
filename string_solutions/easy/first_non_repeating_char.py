96b8ba1006e5c3fb8314faa111f834946b083c50
Abdifatah Mohamed <mofaatah@gmail.com>
2021-08-11 00:04:21 -0500

    Added solution

--- string_solutions/easy/first_non_repeating_char.py
+++ string_solutions/easy/first_non_repeating_char.py
@@ -18,3 +18,25 @@ s3 = "kllm"  # -> k
 s4 = "a"  # -> a
 
 print(first_non_repeating_char(s4))
+
+#########################################################
+
+
+# Using bucket and fixed array size:
+def first_non_repeating_char(string: str) -> str:
+    arr = [0] * 256
+    for i in range(len(string)):
+        if string[i] != " ":
+            num = ord(string[i])
+            arr[num] += 1
+
+    res = ""
+    for i in range(len(string)):
+        if arr[ord(string[i])] == 1:
+            res = string[i]
+            break
+    return res
+
+
+s1 = "aabccd"  # -> b
+print(first_non_repeating_char(s1))
