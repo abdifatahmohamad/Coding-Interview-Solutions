63b6de0500a8c63fd5db3e8b5fc600772445e3a2
Abdifatah Mohamed <mofaatah@gmail.com>
2021-08-14 14:27:13 -0500

    Added solution

---
+++ string_solutions/easy/missing_ch.py
@@ -0,0 +1,29 @@
+def missing_char(word, ch):
+    new_list = []
+    for i in range(len(word)):
+        if word[i] != ch:
+            new_list.append(word[i])
+    return "".join(new_list)
+
+
+print(missing_char("somalia", "a"))
+
+
+# Solving this problem using bucket
+def missing_chars(word: str, target: str) -> str:
+    word = word.lower()
+    missing = [False] * 26
+    for char in word:
+        if char == target:
+            i = ord(char) - ord("a")
+            missing[i] = True
+
+    res = []
+    for ch in word:
+        i = ord(ch) - ord("a")
+        if missing[i] != True:
+            res.append(ch)
+    return "".join(res)
+
+
+print(missing_chars("somalis", "s"))
--- string_solutions/easy/missing_chars.py
+++ string_solutions/easy/missing_chars.py
@@ -55,3 +55,23 @@ def missing_char(word, ch):
 
 
 print(missing_char("somalia", "a"))
+
+
+# Solving this problem using bucket
+def missing_chars(word: str, target: str) -> str:
+    word = word.lower()
+    missing = [False] * 26
+    for char in word:
+        if char == target:
+            i = ord(char) - ord("a")
+            missing[i] = True
+
+    res = []
+    for ch in word:
+        i = ord(ch) - ord("a")
+        if missing[i] != True:
+            res.append(ch)
+    return "".join(res)
+
+
+print(missing_chars("somalis", "s"))
