0084d0559db557760e7c40c9b5ba9967b25fc063
Abdifatah Mohamed <mofaatah@gmail.com>
2021-08-20 22:59:27 -0500

    Added solution

---
+++ linkedList_solutions/hackerrank_solutions/merge_two_lists.py
@@ -0,0 +1,141 @@
+class LinkedList:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+def print_list(head: LinkedList) -> None:
+    while head:
+        print(head.data, end=" -> ")
+        head = head.next
+    print("None")
+
+
+def merge_lists(headA, headB):
+    dummy = LinkedList(None)
+    curr = dummy
+
+    while headA != None and headB != None:
+        if headA.data < headB.data:
+            curr.next = headA
+            headA = headA.next
+        else:
+            curr.next = headB
+            headB = headB.next
+
+        curr = curr.next
+
+    if headA != None:
+        curr.next = headA
+    else:
+        curr.next = headB
+
+    return dummy.next
+
+
+node1 = LinkedList(1)
+node1.next = LinkedList(2)
+node1.next.next = LinkedList(7)
+
+node2 = LinkedList(1)
+node2.next = LinkedList(3)
+
+print_list(merge_lists(node1, node2))
+
+#######################################################################
+
+
+class LinkedList:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+    def __iter__(self):
+        head = self
+        while head:
+            yield head.data
+            head = head.next
+        yield None  # Optional
+
+    def __repr__(self):
+        return " -> ".join(map(str, self))
+
+
+def merge_lists(headA, headB):
+    dummy = LinkedList(None)
+    curr = dummy
+
+    while headA != None and headB != None:
+        if headA.data < headB.data:
+            curr.next = headA
+            headA = headA.next
+        else:
+            curr.next = headB
+            headB = headB.next
+
+        curr = curr.next
+
+    if headA != None:
+        curr.next = headA
+    else:
+        curr.next = headB
+
+    return dummy.next
+
+
+node1 = LinkedList(1)
+node1.next = LinkedList(2)
+node1.next.next = LinkedList(7)
+
+node2 = LinkedList(1)
+node2.next = LinkedList(3)
+
+print(merge_lists(node1, node2))
+
+#######################################################################
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def print_list(head: ListNode) -> None:
+    while head:
+        print(head.val, end=" -> ")
+        head = head.next
+    print("None")
+
+
+def merge_two_lists(list1: ListNode, list2: ListNode) -> ListNode:
+    dummy_head = ListNode(-1)
+    current = dummy_head
+
+    while list1 is not None and list2 is not None:
+        if list1.val < list2.val:
+            current.next = list1
+            list1 = list1.next
+        else:
+            current.next = list2
+            list2 = list2.next
+        current = current.next
+
+    if list1 is not None:
+        current.next = list1
+    else:
+        current.next = list2
+
+    return dummy_head.next
+
+
+if __name__ == "__main__":
+    l1 = ListNode(1, ListNode(2, ListNode(4)))
+    l2 = ListNode(1, ListNode(3, ListNode(4)))
+    # print_list(l1)
+    # print_list(l2)
+
+    merged_lists = merge_two_lists(l1, l2)
+    print_list(merged_lists)
+
+    #######################################################################
