9e9e9a4aef3de4bfefc4b1a02f183e02e2d830e3
Abdifatah Mohamed <mofaatah@gmail.com>
2021-09-07 00:53:52 -0500

    Added solution

--- hashMap_solutions/easy/design_myHashMap.py
+++ hashMap_solutions/easy/design_myHashMap.py
@@ -35,3 +35,43 @@ print(f"Access/get the value of key number 1: {h.get(1)}")
 h.remove(3)
 print("The status of my list after removed key number 3")
 print(h.mapping)  # 44 is removed
+
+
+###############################################
+
+class MyHashMap:
+    def __init__(self):
+        self.mapping = [-1] * 10
+        for i in range(len(self.mapping)):
+            self.mapping[i] = -1
+
+    def get_hash(self, key: str) -> int:
+        hashing = 0
+        for char in key:
+            hashing += ord(char)
+        return hashing % 10
+
+    def put(self, key: str, value: str) -> None:
+        h = self.get_hash(key)
+        self.mapping[h] = value
+
+    def get(self, key: str) -> int:
+        h = self.get_hash(key)
+        return self.mapping[h]
+
+    def remove(self, key: str) -> None:
+        h = self.get_hash(key)
+        self.mapping[h] = -1
+
+
+# Instantiate hashMap object
+h = MyHashMap()
+h.put("Abdifatah", "Junior SWE")
+h.put("Farah", "Mid-level SWE")
+h.put("Mohamed", "Senior SWE")
+print("The status of my list")
+print(h.mapping)
+print(f"Access/get the value of key 'Abdifatah': {h.get('Abdifatah')}")
+h.remove("Farah")
+print("The status of my list after removed key 'Farah' ")
+print(h.mapping)  # 'Mid-level SWE' value is removed
