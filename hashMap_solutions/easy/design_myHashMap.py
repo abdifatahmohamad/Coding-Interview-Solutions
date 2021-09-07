e44d0160d12b298a260a99e705c9c44cd6e82467
Abdifatah Mohamed <mofaatah@gmail.com>
2021-09-07 00:11:37 -0500

    Added solution

--- hashMap_solutions/easy/design_myHashMap.py
+++ hashMap_solutions/easy/design_myHashMap.py
@@ -1,28 +1,37 @@
 class MyHashMap:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
+        # 1:
+        # self.size = 1000001
+        # self.mapping = [-1] * self.size
+
+        # 2:
+        # self.mapping = [-1 for _ in range(1000001)]
+
+        # 3:
+        self.mapping = [-1] * 10
+        for i in range(len(self.mapping)):
+            self.mapping[i] = -1
 
     def put(self, key: int, value: int) -> None:
-        """
-        value will always be non-negative.
-        """
+        self.mapping[key] = value
 
     def get(self, key: int) -> int:
-        """
-        Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
-        """
+        return self.mapping[key]
 
     def remove(self, key: int) -> None:
-        """
-        Removes the mapping of the specified value key if this map contains a mapping for the key
-        """
+        self.mapping[key] = -1
 
 
-# Your MyHashMap object will be instantiated and called as such:
-# obj = MyHashMap()
-# obj.put(key,value)
-# param_2 = obj.get(key)
-# obj.remove(key)
+# Instantiate hashMap object
+h = MyHashMap()
+h.put(1, 22)
+h.put(2, 33)
+h.put(3, 44)
+h.put(4, 55)
+print("The status of my list")
+print(h.mapping)
+print(f"Access/get the value of key number 1: {h.get(1)}")
+h.remove(3)
+print("The status of my list after removed key number 3")
+print(h.mapping)  # 44 is removed
