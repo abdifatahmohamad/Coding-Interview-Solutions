ef950d004bace4cf3262d7b1ac04026c704fb796
Abdifatah Mohamed <mofaatah@gmail.com>
2021-09-21 17:46:54 -0500

    Added solution

--- array_solutions/easy/twoSum.py
+++ array_solutions/easy/twoSum.py
@@ -1,7 +1,7 @@
 # O(N^2) Time, O(1) Space
 def twoNumberSum(nums, target):
     for i in range(len(nums)):
-        for j in range(1, len(nums)):
+        for j in range(i + 1, len(nums)):
             if nums[i] + nums[j] == target:
                 return [i, j]
     return [-1, -1]
@@ -47,16 +47,35 @@ def twoNumberSum(array, target):
 
 
 # If asked to return indices instead of nums
-def twoNumberSum(array, target):
-    mapping = {}
-    for i in range(len(array)):
-        num = array[i]
-        complement = target - num
-        if num in mapping:
-            return [mapping[num], i]
+def sum(nums, target):
+    seen = {}
+    for i in range(len(nums)):
+        complement = target - nums[i]
+        if complement not in seen:
+            seen[nums[i]] = i
         else:
-            mapping[complement] = i
-    return []
+            return [seen[complement], i]
+    return seen
+
+
+print(sum([2, 15, 11, 7], 9))
+
+# Complement = target - num
+# 9 - 2 = 7 -> is 7 in seen => NO, store 2 in hashmap
+# 9 - 15 = -6 -> is -6 in seen => NO, store 15 in hashmap
+# 9 - 11 = -2 -> is -2 in seen => NO, store 11 in hashmap
+# 9 - 7 = 2 -> is 2 in seen => YES, return it its index in hashmap and the index of nums[i]
+
+# seen = {
+# 2 = 0
+# 15  = 1
+# 11 - 2
+# }
+
+# for loop to end of numbers array
+# determine complement
+# if complement is in dictionary, return value of complement and i
+# otherwise, new key-value pair in dicitonary, number -> i
 
 
 array = [3, 5, -4, 8, 11, 1, -1, 6]
