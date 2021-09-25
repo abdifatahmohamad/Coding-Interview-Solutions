052e60f52ad408c6dff9b541f6bbbeec4c70a898
Abdifatah Mohamed <mofaatah@gmail.com>
2021-09-25 00:09:07 -0500

    Added solution

--- leetcode_solutions/arrays-easy/1.1_twoSum.py
+++ leetcode_solutions/arrays-easy/1.1_twoSum.py
@@ -5,6 +5,7 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Array sorted started at index 0
         nums.sort()
         left, right = 0, len(nums) - 1
         while left < right:
@@ -19,18 +20,35 @@ class Solution:
 
 
 # Solution 2 using hash map
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        mapping = {}
-        for i in range(len(nums)):
-            num = nums[i]
-            complement = target - num
-            if num in mapping:
-                return [mapping[num], i]
-            else:
-                mapping[complement] = i
+def twoSum(nums, target):
+    seen = {}
+    for i in range(len(nums)):
+        complement = target - nums[i]
+        if complement not in seen:
+            seen[nums[i]] = i
+        else:
+            return [seen[complement], i]
+    return seen
 
-        return [-1, -1]
+
+print(sum([2, 15, 11, 7], 9))
+
+# Complement = target - num
+# 9 - 2 = 7 -> is 7 in seen => NO, store 2 in hashmap
+# 9 - 15 = -6 -> is -6 in seen => NO, store 15 in hashmap
+# 9 - 11 = -2 -> is -2 in seen => NO, store 11 in hashmap
+# 9 - 7 = 2 -> is 2 in seen => YES, return it its index in hashmap and the index of nums[i]
+
+# seen = {
+# 2 = 0
+# 15  = 1
+# 11 - 2
+# }
+
+# Loop through till the end of numbers array
+# determine complement
+# if complement is in dictionary, return value of complement and i
+# otherwise, new key-value pair in dicitonary, number -> i
 
 
 solution = Solution()
