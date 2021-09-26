068ec39b8c8debae51f9855117d9096757c9fa3d
Abdifatah Mohamed <mofaatah@gmail.com>
2021-09-26 00:28:42 -0500

    Added solution

---
+++ hashMap_solutions/easy/students_grades.py
@@ -0,0 +1,15 @@
+# Insert 2D array into a hashmap
+students = [['Hassan', 92], ['Abdifatah', 95],
+            ['Hassan', 88], ['Abdifatah', 76],
+            ['Abdifatah', 81], ['Hassan', 73]]
+
+# Output: {'Hassan':[92], 'Abdifatah':[95]}
+
+mapping = {}
+
+for name, grade in students:
+    if name not in mapping:
+        mapping[name] = []
+    mapping[name].append(grade)
+
+print(mapping)
