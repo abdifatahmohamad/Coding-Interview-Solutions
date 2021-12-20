dbcaf4a7964f824cec17ccff8a6c8c4f999aca2e
Abdifatah Mohamed <mofaatah@gmail.com>
2021-12-20 13:57:02 -0600

    Added solution

---
+++ string_solutions/easy/next_greatest_target.py
@@ -0,0 +1,32 @@
+# https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+
+def next_greatest_letter(letters, target) -> str:
+    for i in range(len(letters)):
+        if ord(letters[i]) > ord(target):
+            return letters[i]
+    return letters[0]
+
+
+# letters = ['a', 'b']
+# target = 'z'
+# # Output: a
+
+
+# letters = ['c', 'f', 'j']
+# target = 'a'
+# # Output: c
+
+# letters = ['c', 'f', 'j']
+# target = 'c'
+# # Output: f
+
+letters = ['c', 'f', 'j']
+target = 'j'
+# Output: c
+
+# letters = ['c', 'f', 'j']
+# target = 'd'
+# # Output: f
+
+
+print(next_greatest_letter(letters, target))
