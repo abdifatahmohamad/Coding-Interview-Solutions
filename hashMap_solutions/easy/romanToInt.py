e5d182cd3ea1dde10cd3c4c999541bb0559c6442
Abdifatah Mohamed <mofaatah@gmail.com>
2022-03-15 07:13:56 -0500

    Added solution

---
+++ hashMap_solutions/easy/romanToInt.py
@@ -0,0 +1,29 @@
+# https://www.youtube.com/watch?v=3jdxYj3DD98
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        res = 0
+        for i in range(len(s)):
+            # i + 1 < len(s) checks if next value is in bounds (range)
+            if i + 1 < len(s) and roman[s[i]] < roman[s[i + 1]]:
+                res -= roman[s[i]]
+            else:
+                res += roman[s[i]]
+        return res
+
+
+solution = Solution()
+# string = "III"
+# string = "MCMXCIV"
+# string = "LVIII"
+string = "MCMXCVI"    # ---> Output: 1996
+print(solution.romanToInt(string))
