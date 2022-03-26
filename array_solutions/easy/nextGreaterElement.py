28d5d582286e88cbe1c1ccbc9c771217609d6710
Abdifatah Mohamed <mofaatah@gmail.com>
2022-03-26 00:53:39 -0500

    Added solution

--- array_solutions/easy/nextGreaterElement.py
+++ array_solutions/easy/nextGreaterElement.py
@@ -1,10 +1,10 @@
 from typing import List
 
 
-# Brute Force Solution O(n^2) Time || O(1) Space
+# Brute Force Solution O(N) Time || O(1) Space
 class Solution:
     def nextGreaterElement(self, nums: List[int]) -> List[int]:
-        ans = [-1] * (len(nums))
+        ans = [0] * (len(nums))
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
                 if nums[j] > nums[i]:
@@ -13,10 +13,34 @@ class Solution:
         return ans
 
 
-solution = Solution()
-# nums = [11, 13, 21, 3]
-nums = [9, 1, 20, 2, 8, 6, 4, 2]
+##########################################################
+# Optimal Solution O(N) Time || O(N) Space Using stack DS
 
+class Solution:
+    def nextGreaterElement(self, nums: List[int]) -> List[int]:
+        res = [-1] * len(nums)
+
+        # create an empty stack
+        stack = []
+
+        # do for each element
+        for i in range(len(nums)):
+
+            # loop till we have a greater element on top or stack becomes empty.
+
+            # Keep popping elements from the stack smaller than the current
+            # element, and set their next greater element to the current element
+            while stack and nums[stack[-1]] < nums[i]:
+                res[stack[-1]] = nums[i]
+                stack.pop()
+
+            # push current "index" into the stack
+            stack.append(i)
+
+        return res
+
+
+solution = Solution()
+nums = [11, 13, 21, 3]
 # Output: [13 ,21, -1, -`1]
-# Output: [20, 20, -1, 8, -1, -1, -1, -1]
 print(solution.nextGreaterElement(nums))
