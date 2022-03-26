e96f0fc3c6684bf5e58f490739ebeb32f7861bbc
Abdifatah Mohamed <mofaatah@gmail.com>
2022-03-26 05:48:01 -0500

    Added solution

--- hashMap_solutions/easy/nextGreaterElementILeetcode.py
+++ hashMap_solutions/easy/nextGreaterElementILeetcode.py
@@ -1,5 +1,7 @@
 from typing import List
 
+# O(M * N) Time || O(N) Space
+
 
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
@@ -18,6 +20,28 @@ class Solution:
                     idx = d[nums2[i]]
                     ans[idx] = nums2[j]
                     break
+        return ans
+
+##################################################
+# O(M + N) Time || O(N) Space
+
+
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        d = {}
+        for idx, num in enumerate(nums1):
+            d[num] = idx
+        stack = []
+        ans = [-1] * (len(nums1))
+        for i in range(len(nums2)):
+            curr = nums2[i]
+            while stack and curr > stack[-1]:
+                element = stack.pop()
+                idx = d[element]
+                ans[idx] = curr
+
+            if curr in d:
+                stack.append(curr)
         return ans
 
 
