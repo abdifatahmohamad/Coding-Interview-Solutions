eca1418609a3553c9b22d7f9d0f0005d1920a7cb
Abdifatah Mohamed <mofaatah@gmail.com>
2022-04-10 03:57:38 -0500

    Added string solution

---
+++ string_solutions/easy/maximum_occurrence_character.py
@@ -0,0 +1,20 @@
+from collections import OrderedDict
+# CodePath easy problem
+
+
+def maximum_occurrence_character(s):
+    frequency = {}
+    for char in s:
+        frequency[char] = frequency.get(char, 0) + 1
+
+    max_occurrence = float("-inf")
+    res = ""
+    for char, value in frequency.items():
+        if value > max_occurrence:
+            max_occurrence = value
+            res = char
+
+    return res
+
+
+print(maximum_occurrence_character("abbbaacc"))
