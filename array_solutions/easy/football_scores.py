9ff9330099aac1da817201b5fc3eb6f65f5e47f0
Abdifatah Mohamed <mofaatah@gmail.com>
2022-04-21 06:23:49 -0500

    Added solution

---
+++ array_solutions/easy/football_scores.py
@@ -0,0 +1,54 @@
+# Brute force solution:
+from bisect import bisect_right  # For binary search.
+
+
+def counts(teamA, teamB):
+    res = []
+    for b in teamB:
+        count = 0
+        for a in teamA:
+            count += a <= b
+        res.append(count)
+    return res
+
+
+# Short version using list comprehension:
+[sum(lst1 <= lst2 for lst1 in teamA) for lst2 in teamB]
+
+
+# O(NLOGN) Time || O(N) Space
+def counts(teamA, teamB):
+    lst1_sorted = sorted(teamA)
+    lst2_sorted = sorted(teamB)
+    '''
+    res = []
+    for val in lst2_sorted:
+        res.append(binary_search(lst1_sorted, val))
+    return res
+    '''
+    # Short version with list comprehension
+    return [binary_search(lst1_sorted, val) for val in lst2_sorted]
+
+
+def binary_search(nums, val):
+    left, right = 0, len(nums)
+    while left < right:
+        mid = (right + left) // 2
+        if nums[mid] <= val:
+            left = mid + 1
+        else:
+            right = mid
+
+    return right
+
+
+print(counts([1, 2, 3], [2, 4]))
+print(counts([1, 4, 2, 4], [3, 5]))  # [2, 4]
+print(counts([2, 10, 5, 4, 8], [3, 1, 7, 8]))  # [1, 0, 3, 4]
+
+
+# Using bisect_right imported from binary search package
+lst1_sorted = sorted(teamA)  # NlogN
+lst2_sorted = sorted(teamB)  # MlogM
+
+res = [bisect_right(lst1_sorted, val) for val in lst2_sorted]  # MlogN
