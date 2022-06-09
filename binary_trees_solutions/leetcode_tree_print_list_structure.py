dadbc4f10311646f5188e9f2a11fef0740422881
Abdifatah Mohamed <mofaatah@gmail.com>
2022-06-09 01:34:14 -0500

    Leetcode print list structure

--- binary_trees_solutions/leetcode_tree_print_list_structure.py
+++ binary_trees_solutions/leetcode_tree_print_list_structure.py
@@ -1,7 +1,29 @@
+import collections
+
+
 class TreeNode(object):
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left, self.right = left, right
+
+
+def print_tree_bfs(root):
+    if not root:
+        return
+    queue = collections.deque([root])
+    res = []
+    while queue:
+        level_size = len(queue)
+        for _ in range(level_size):
+            curr = queue.popleft()
+            res.append(curr.val)
+
+            if curr.left:
+                queue.append(curr.left)
+
+            if curr.right:
+                queue.append(curr.right)
+    return res
 
 
 if __name__ == "__main__":
@@ -18,3 +40,4 @@ if __name__ == "__main__":
     tree.left = TreeNode(9, None, None)
     tree.right = TreeNode(20, TreeNode(15, None, None),
                           TreeNode(7, None, None))
+    print(print_tree_bfs(tree))  # [3, 9, 20, 15, 7]
