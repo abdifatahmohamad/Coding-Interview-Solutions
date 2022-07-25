15abf0cdd6b6eb297e2a80f5bd8b2b63f1bdfc9b
Abdifatah Mohamed <48977326+abdifatahdev@users.noreply.github.com>
2022-07-25 17:47:22 -0500

    Time: 72 ms (31.59%), Space: 14.3 MB (26.01%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
@@ -3,14 +3,9 @@ class Solution:
         if len(nums) <= 1:
             return
         
-        result = [0] * len(nums)
-        for i in range(len(nums)):
-            if i == 0:
-                result[0] = nums[0]
-            
-            curr = nums[i]
-            prev = result[i - 1]
-            total =  curr + prev
-            result[i] = total
+        result = [nums[0]]
+        for i in range(1, len(nums)):
+            total = nums[i] + result[-1]
+            result.append(total)
             
         return result
