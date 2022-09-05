6bcd2aa503189a60d674d557f1bf76c816f34eb9
Abdifatah Mohamed <48977326+abdifatahdev@users.noreply.github.com>
2022-09-04 20:56:38 -0500

    Time: 241 ms (10.55%), Space: 19.9 MB (9.29%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,12 +1,14 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        mapping = defaultdict(list)
+        mapping = {}
         for word in strs:
             alpha = [0 for _ in range(26)]
             for c in word:
                 alpha[ord(c) - ord('a')] += 1
                 
             key = tuple(alpha)
+            if key not in mapping:
+                mapping[key] = []
             mapping[key].append(word)
             
         return mapping.values()
