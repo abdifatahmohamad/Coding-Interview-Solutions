88619778ceb3776c3943147a8e4c7c538ae91e4f
Abdifatah Mohamed <mofaatah@gmail.com>
2022-11-18 13:18:39 -0600

    Added reversals solution

--- array_solutions/easy/reversals.py
+++ array_solutions/easy/reversals.py
@@ -81,7 +81,33 @@ from typing import List
 # print(reverse_numbers(nums))  # [7, 6, 5, 4, 3, 2, 1]
 
 ############################################################################
+# Reverse words of string: Leetcode: 557. Reverse Words in a String III
+# I have being asked this question by ESRI
+'''
+def reverse_string_words(s: str) -> str:
+    st = s.split()
+    for i in range(len(st)):
+        st[i] = swap(st[i])
+    return " ".join(st)
 
+
+def swap(word):
+    w = list(word)
+    l, r = 0, len(w) - 1
+    while l < r:
+        w[l], w[r] = w[r], w[l]
+        l += 1
+        r -= 1
+    return "".join(w)
+
+
+print(reverse_string_words("Welcome to this ESRI!"))
+# Output: emocleW ot siht !IRSE"
+
+'''
+
+
+############################################################################
 # def reverse_string(s: str) -> str:
 #     res = ""
 #     for ch in s:
