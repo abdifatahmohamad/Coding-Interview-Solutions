852482870453da9ef2a68eddd84880a004c6165d
Abdifatah Mohamed <48977326+abdifatahdev@users.noreply.github.com>
2022-11-26 16:47:42 -0600

    Time: 2620 ms (67.48%), Space: 31.7 MB (35.33%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
@@ -2,16 +2,22 @@ class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
         res = [0] * len(nums)
         k = 0
+        # Traverse left to right and push
+        # all elements which are stricktly less than pivot
         for i in range(len(nums)):
             if nums[i] < pivot:
                 res[k] = nums[i]
                 k += 1
-
+        
+        # Traverse 2nd time left to right and push
+        # all elements which are stricktly equal to pivot
         for i in range(len(nums)):
             if nums[i] == pivot:
                 res[k] = nums[i]
                 k += 1
 
+         # Traverse 3rd time left to right and push
+        # all elements which are stricktly > to pivot
         for i in range(len(nums)):
             if nums[i] > pivot:
                 res[k] = nums[i]
