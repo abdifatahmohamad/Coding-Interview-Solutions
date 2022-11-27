82bd351197765bb37d9f0296813b91b38fb24831
Abdifatah Mohamed <48977326+abdifatahdev@users.noreply.github.com>
2022-11-27 08:24:32 -0600

    Time: 131 ms (55.82%), Space: 15.4 MB (14.09%) - LeetHub

--- 0942-di-string-match/0942-di-string-match.py
+++ 0942-di-string-match/0942-di-string-match.py
@@ -1,6 +1,7 @@
 class Solution:
     def diStringMatch(self, s: str) -> List[int]:
-        res = [0] * int(len(s) +  1)
+        res = [0 for i in range(len(s) +  1)]
+        # res = [0] * int(len(s) +  1)
         low, high = 0, len(s)
         for i in range(len(s)):
             if s[i] == 'I':
