81ef795a300064068ea588994c0965ffeeafee46
Abdifatah Mohamed <48977326+abdifatahdev@users.noreply.github.com>
2022-12-06 23:50:59 -0600

    Time: 6 ms (30.71%), Space: 43.4 MB (82.35%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,34 +1,15 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        // Map<Integer, Integer> mp = new HashMap<>();
-        // for (int i = 0; i < nums; i++){
-        //     if (mp.containsKey(arr[i]))
-        //     {
-        //         mp.put(arr[i], mp.get(arr[i]) + 1);
-        //     }
-        //     else
-        //     {
-        //         mp.put(arr[i], 1);
-        //     }
-        // }
-        
-        // Shorter version
-        Map<Integer,Integer> mp = new HashMap<>();
-        for(int n: nums){
-           mp.put(n, mp.getOrDefault(n, 0) + 1);
+        HashMap<Integer, Integer> mp = new HashMap<Integer, Integer>();
+        for(int i=0; i<nums.length; i++){
+            mp.put(nums[i], i);
         }
-        
-        int missing = 0;
-        for(int i = 1; i < nums.length + 1; i++){
+        for(int i=0; i<=nums.length; i++){
             if(!mp.containsKey(i)){
-                missing = i;
-            }
+                return i;
+            } 
         }
-        // for i in range(1, len(nums) + 1):
-        //     if i not in lookup:
-        //         missing = i
-
-        return missing;
+        return 0;
         
     }
 }
