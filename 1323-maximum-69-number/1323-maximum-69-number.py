b2dd8f67018ce9fac962f3922b1960ac31147689
Abdifatah Mohamed <48977326+abdifatahdev@users.noreply.github.com>
2022-12-11 12:22:10 -0600

    Time: 49 ms (68.94%), Space: 13.8 MB (54.69%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.py
+++ 1323-maximum-69-number/1323-maximum-69-number.py
@@ -1,18 +1,10 @@
 class Solution:
     def maximum69Number (self, num: int) -> int:
         res = [n for n in str(num)]
-        max_val = float("-Inf")
-        for i in range(len(res)):
-            prev = ''
-            if res[i] == '9':
-                res[i] = '6'
-                prev = '9'
-            elif res[i] == '6':
+        for i, c in enumerate(res):
+            if c == '6':
                 res[i] = '9'
-                prev = '6'
-            max_val = max(max_val, int("".join(res)))
-            res[i] = prev
-            max_val = max(max_val, int("".join(res)))
-        return max_val
+                break
+        return int("".join(res))
 
         
