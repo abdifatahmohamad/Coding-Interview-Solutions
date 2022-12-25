81a5657194344834a87aa9f54b91e908d186ec55
Abdifatah Mohamed <48977326+abdifatahdev@users.noreply.github.com>
2022-12-25 10:06:47 -0600

    Time: 856 ms (81.58%), Space: 15.8 MB (10.77%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.py
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.py
@@ -2,13 +2,12 @@ class Solution:
     def checkValid(self, matrix: List[List[int]]) -> bool:
         rowsSet = collections.defaultdict(set)
         colsSet = collections.defaultdict(set)
-        rows, cols = len(matrix), len(matrix[0])
-        for r in range(rows):
-            for c in range(cols):
-                value = matrix[r][c]
-                if value in rowsSet[r] or value in colsSet[c]:
+        n = len(matrix)
+        for r in range(n):
+            for c in range(n):
+                if matrix[r][c] in rowsSet[r] or matrix[r][c] in colsSet[c]:
                     return False
-                rowsSet[r].add(value)
-                colsSet[c].add(value)
+                rowsSet[r].add(matrix[r][c])
+                colsSet[c].add(matrix[r][c])
         return True
         
