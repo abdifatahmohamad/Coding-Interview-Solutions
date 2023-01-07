4254a7d00b56967c73c61d3b10a4eabc09441425
Abdifatah Mohamed <48977326+abdifatahdev@users.noreply.github.com>
2023-01-07 17:31:27 -0600

    Time: 136 ms (93.49%), Space: 14.5 MB (92.10%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
@@ -1,11 +1,16 @@
 class Solution:
-    def minDeletionSize(self, s: List[str]) -> int:
-        rows, cols = len(s) - 1, len(s[0])
-        min_deletions = 0
-        for i in range(cols):  # Iterate through the characters in each string
-            for j in range(rows):  # Compare each character to the character in the same position in the next string
-                if s[j][i] > s[j + 1][i]:  # If the character in the later string is lexicographically smaller
-                    min_deletions += 1  # Increment min_deletions
-                    break  # Continue to the next character
-        return min_deletions
+    def minDeletionSize(self, strs: List[str]) -> int:
+        if strs is None or len(strs) == 0:
+            return 0
+
+        deletions = 0
+        for col in range(len(strs[0])):
+            char = strs[0][col]
+            for word in range(len(strs)):
+                if strs[word][col] < char:
+                    deletions += 1
+                    break
+                char = strs[word][col]
+
+        return deletions
         
