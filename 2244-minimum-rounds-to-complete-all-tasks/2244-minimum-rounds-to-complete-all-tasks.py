5b55fcc2e1d3c0adec252ea87c1cad617b0eece2
Abdifatah Mohamed <48977326+abdifatahdev@users.noreply.github.com>
2023-01-08 13:39:09 -0600

    Time: 1048 ms (84.52%), Space: 28.1 MB (80.98%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
@@ -4,13 +4,14 @@ class Solution:
         for n in tasks:
             frequency[n] = frequency.get(n, 0) + 1
 
-        res = 0
+        count = 0
         for key, value in frequency.items():
             if value == 1:
                 return -1
-            if value % 3 == 0:
-                res += value // 3
-            if value % 3 == 1 or value % 3 == 2:
-                res += value // 3 + 1
-        return res
+            else:
+                count += value // 3
+                if value % 3 != 0:
+                    count += 1
+        
+        return count
         
