b8cf6f8433280a3a3493b507437fae709e1e1aa0
Abdifatah Mohamed <mofaatah@gmail.com>
2023-01-14 10:12:40 -0600

    Added hashmap solution

--- hashMap_solutions/easy/romanToInt.py
+++ hashMap_solutions/easy/romanToInt.py
@@ -1,29 +1,31 @@
-# https://www.youtube.com/watch?v=3jdxYj3DD98
+def romanToInt(s: str) -> int:
+    roman = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+    result = 0
+    for i in range(len(s)):
+        curr = s[i]
+        # nxt = s[i + 1]
+        # i + 1 < len(s) checks if there's a character that comes after it, if it's true we compare the values
+        # i + 1 < len(s) checks if next value is in bounds (range)
+        # If current character followed by a large character, then the current is going to be negative
+        # if (i + 1 < len(s)) and roman[curr] < roman[nxt]:
+        if i + 1 < len(s) and roman[curr] < roman[s[i + 1]]:
+            # Subtract result from current
+            result -= roman[curr]
+        else:
+            # Add result from current
+            result += roman[curr]
+    return result
 
-class Solution:
-    def romanToInt(self, s: str) -> int:
-        roman = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000
-        }
-        res = 0
-        for i in range(len(s)):
-            # i + 1 < len(s) checks if next value is in bounds (range)
-            if i + 1 < len(s) and roman[s[i]] < roman[s[i + 1]]:
-                res -= roman[s[i]]
-            else:
-                res += roman[s[i]]
-        return res
 
-
-solution = Solution()
-# string = "III"
-# string = "MCMXCIV"
-# string = "LVIII"
-string = "MCMXCVI"    # ---> Output: 1996
-print(solution.romanToInt(string))
+print(romanToInt("LVIII"))
+print(romanToInt("XXVII"))
+print(romanToInt("CMXCVIII"))
+print(romanToInt("MCMXCIV"))
