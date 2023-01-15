aa1a9e7a0dcf407571064b80e8a7ea841ee253b1
Abdifatah Mohamed <48977326+abdifatahdev@users.noreply.github.com>
2023-01-15 17:20:48 -0600

    Time: 34 ms (74.44%), Space: 14 MB (9.79%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -2,7 +2,8 @@ class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
         bucket = [0 for _ in range(26)]
         for c in sentence:
-            if 'a' <= c <= 'z':
+            # if 'a' <= c <= 'z':
+            if 'a' <= c or 'z' <= c:
                 bucket[ord(c) - ord('a')] = True
         
         for ch in bucket:
