d27c7a0af2abfd32313ef47f20d6974fe06ccbfe
Abdifatah Mohamed <48977326+abdifatahdev@users.noreply.github.com>
2023-01-15 09:21:33 -0600

    Time: 32 ms (82.85%), Space: 13.8 MB (51.63%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -1,12 +1,33 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
+        '''
         alphabet = "abcdefghijklmnopqrstuvwxyz"
         mapping = {}
         for char in sentence:
             mapping[char] = True
         
         return len(mapping) == len(alphabet)
-            
+        
+        '''
+        # Set solution:
+        alphabet = "abcdefghijklmnopqrstuvwxyz"
+        found = set()
+        for char in sentence:
+            found.add(char)
+        
+        return len(found) == len(alphabet)
+        
+        
+        # Bucket solution:
+        '''
+        bucket = [0 for _ in range(26)]
+        for char in sentence:
+            index = ord(c) - ord('a')
+            bucket[index] += True
+        
+        return len(bucket) == len(sentence)
+        
+        '''   
             
         
         
