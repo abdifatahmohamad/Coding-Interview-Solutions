8a32af3ca0fc0661a8b7095933ee2c2bc497973a
Abdifatah Mohamed <48977326+abdifatahdev@users.noreply.github.com>
2023-01-15 19:19:20 -0600

    Time: 47 ms (77.41%), Space: 14 MB (15.06%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.py
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.py
@@ -1,11 +1,9 @@
 class Solution:
     def arithmeticTriplets(self, nums: List[int], diff: int) -> int:
         seen = set(nums)
-        one_time_diff = [(n - diff) for n in nums]
-        two_time_diff = [(n - diff * 2) for n in nums]
         unique = 0
-        for i in range(len(nums)):
-            if one_time_diff[i] in seen and two_time_diff[i] in seen:
+        for n in nums:
+            if (n - diff) in seen and (n - diff * 2) in seen:
                 unique += 1
 
         return unique
