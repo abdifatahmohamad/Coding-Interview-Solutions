a4fa2b8a7236200152a32056d2c6312a00e44022
Abdifatah Mohamed <48977326+abdifatahdev@users.noreply.github.com>
2023-01-21 13:42:14 -0600

    Time: 84 ms (79.96%), Space: 13.8 MB (61.89%) - LeetHub

--- 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.py
+++ 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.py
@@ -1,15 +1,13 @@
 class Solution:
     def countKDifference(self, nums: List[int], k: int) -> int:
-        nums_dict, count = {}, 0
-        for num in nums:
-            if num not in nums_dict:
-                nums_dict[num] = 1
-            else:
-                nums_dict[num] += 1
+        mapping = {}
+        for n in nums:
+            mapping[n] = mapping.get(n, 0) + 1
 
-        for num in nums:
-            if num + k in nums_dict:
-                count += nums_dict[num + k]
+        count = 0
+        for n in nums:
+            if n + k in mapping:
+                count += mapping[n + k]
         return count
 
         
