bdf05b09b4aab1afe378bf59c6b2b372ab520fbf
Abdifatah Mohamed <48977326+abdifatahdev@users.noreply.github.com>
2023-01-21 13:52:57 -0600

    Time: 69 ms (86.12%), Space: 13.8 MB (61.89%) - LeetHub

--- 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.py
+++ 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.py
@@ -9,4 +9,38 @@ class Solution:
                 count += mapping[complement]
         return count
 
-        
+
+'''
+mapping = {
+    3: 1,
+    2: 1,
+    1: 1,
+    5: 1,
+    4: 1
+}
+
+count = 0
+k = 2
+nums = [3, 2, 1, 5, 4]
+complement = 3 + 2 in mapping ==> yes
+mapping[complement] = 1
+count += mapping[complement]
+count = 1
+
+
+complement = 2 + 2 in mapping ==> yes
+count += mapping[complement]
+count = 2
+
+
+complement = 1 + 2 in mapping ==> yes
+count += mapping[complement]
+count = 3
+
+
+complement = 5 + 2 in mapping ==> no
+
+complement = 4 + 2 in mapping ==> no
+
+return count
+'''
