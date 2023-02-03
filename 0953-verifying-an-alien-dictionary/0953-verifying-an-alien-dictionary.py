61e7a1d6c4871ce0f846d17ccd9c21bbfe173a3e
Abdifatah Mohamed <48977326+abdifatahdev@users.noreply.github.com>
2023-02-02 19:40:07 -0600

    Time: 44 ms (44.76%), Space: 13.8 MB (77.53%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
@@ -3,13 +3,22 @@ class Solution:
         mapping = {c: i for i, c in enumerate(order)}
         
         for i in range(len(words) - 1):
-            w1, w2 = words[i], words[i + 1]
-            for j in range(len(w1)):
-                if j == len(w2):
+            word1, word2 = words[i], words[i + 1]
+            for j in range(len(word1)):
+                # if word2 is brefix of word1, return False
+                if j == len(word2):
                     return False
-                if w1[j] != w2[j]:
-                    if mapping.get(w1[j]) > mapping.get(w2[j]):
+                
+                # Look for first different character
+                word1Char, word2Char = word1[j], word2[j]
+                if word1Char != word2Char:
+                    # Verify that character in word1 comes before character in word2
+                    # in our order in mapping dictionary
+                    # If character in word2 suppose to be greater than word1
+                    if mapping.get(word1Char) > mapping.get(word2Char):
                         return False
+                    # Character in word1 suppose to be greater than word2
+                    # stop comparing the two words, we found the first different character
                     break
                     
         return True
