26c32758f2a8db925ed08d6537d8d1ab996f526f
Abdifatah Mohamed <48977326+abdifatahdev@users.noreply.github.com>
2023-02-05 08:01:27 -0600

    Time: 112 ms (84.44%), Space: 15.1 MB (74.41%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,26 +1,32 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        res = []
-        mapping = defaultdict(int)
-        for c in p:
-            mapping[c] += 1
-        counter = len(mapping)
-        left, right = 0, 0
-        while right < len(s):
-            c = s[right]
-            if c in mapping:
-                mapping[c] -= 1
-                if mapping[c] == 0:
-                    counter -= 1
-            right += 1
-            while counter == 0:
-                temp = s[left]
-                if temp in mapping:
-                    mapping[temp] += 1
-                    if mapping[temp] > 0:
-                        counter += 1
-                if right - left == len(p):
-                    res.append(left)
-                left += 1
+        # Not going to be an anagram if length of p > length s
+        if len(p) > len(s):
+            return []
+
+        pCount, sCount = {}, {}
+        for i in range(len(p)):
+            pCount[p[i]] = pCount.get(p[i], 0) + 1
+            sCount[s[i]] = sCount.get(s[i], 0) + 1
+
+        # Compare two hashmaps, if they are equal assign the result to list with zero inserted
+        res = [0] if pCount == sCount else []
+        l = 0
+        for r in range(len(p), len(s)):
+            # Add character at index r to our hash map
+            sCount[s[r]] = sCount.get(s[r], 0) + 1
+            # Remove the character that was at left index
+            sCount[s[l]] -= 1
+
+            # If count of character at left index becomes zero, remove/pop off from hashmap
+            if sCount[s[l]] == 0:
+                # Pop off that value from hashmap
+                sCount.pop(s[l])
+            # Increment left pointer
+            l += 1
+
+            # If two hashmaps are equal, append left index to the result
+            if sCount == pCount:
+                res.append(l)
         return res
         
