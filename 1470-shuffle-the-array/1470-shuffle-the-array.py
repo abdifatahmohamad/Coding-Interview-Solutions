b719aba443bf9b1040531b7b1583f828e2c73714
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2023-02-08 13:08:12 -0600

    Time: 69 ms (25.48%), Space: 14.2 MB (33.22%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.py
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -1,13 +1,11 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        arr1 = nums[:len(nums) // 2]
-        arr2 = nums[len(nums) // 2:]
-
         res = []
-        i = 0
-        while i < n:
-            res.append(arr1[i])
-            res.append(arr2[i])
-            i += 1
-        return res 
+        for i in range(n):
+            curr_first_half = nums[i]
+            curr_2nd_half = nums[n + i]
+            res.append(curr_first_half)
+            res.append(curr_2nd_half)
+            
+        return res
         
