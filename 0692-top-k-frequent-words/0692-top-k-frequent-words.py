57b9f0ce8b0e20ce8bfdc60a1b4c50818fddf9cd
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2023-02-09 16:33:01 -0600

    Time: 64 ms (43.94%), Space: 13.8 MB (92.27%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,16 +1,25 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        mapping = Counter(words)
-        res = []
+        mapping = {}
+        for c in words:
+            mapping[c] = mapping.get(c, 0) + 1
+
+        # Find top k elements
+        result = []
         for i in range(k):
-            max_frequency = 0
-            max_word = ""
-            for word, frequency in mapping.items():
-                if frequency > max_frequency or (frequency == max_frequency and word < max_word):
-                    max_frequency = frequency
-                    max_word = word
-            res.append(max_word)
+            max_word = find_top_word(mapping)
+            result.append(max_word)
             del mapping[max_word]
 
-        return res
+        return result
+
+# Find top word from the dictionary
+def find_top_word(mapping):
+    max_freq = 0
+    max_word = ""
+    for word, frequency in mapping.items():
+        if frequency > max_freq or (frequency == max_freq and word < max_word):
+            max_freq = frequency
+            max_word = word
+    return max_word
         
