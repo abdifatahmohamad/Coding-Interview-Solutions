0db683d555c387221dd82260c42a0fd072a5b6d3
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2023-02-12 12:33:58 -0600

    Time: 47 ms (97.06%), Space: 14 MB (23.45%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.py
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.py
@@ -1,8 +1,11 @@
 class Solution:
     def maximumWealth(self, accounts: List[List[int]]) -> int:
         richest = float("-Inf")
+        wealth = 0
         for i in range(len(accounts)):
-            wealth = sum(accounts[i])
+            for j in range(len(accounts[i])):
+                wealth += accounts[i][j]
             richest = max(richest, wealth)
+            wealth = 0
         return richest
         
