ed995f57ba85a97efb66e2e848efb7a42d73f2e3
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2023-02-12 16:05:38 -0600

    Time: 532 ms (81.65%), Space: 14 MB (56.77%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -3,32 +3,40 @@ class Solution:
         max_length = 0
         res = ""
         for i in range(len(s)):
-            # 1. Take care of odd length palindrome
-            l = r = i
-            # while l and r are in bound, and it's palindrome (l and r are equal)
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                # Check the length of the palindrome
-                if (r - l + 1) > max_length:
-                    # Update the result
-                    res = s[l: r + 1]
-                    # Update max mlength
-                    max_length = r - l + 1
-                # Expand pointers
-                l -= 1
-                r += 1
+            # setup pointers
+            l, r = expand_odd_length(s, i, i)
+            # Check the length of the palindrome
+            if (r - l + 1) > max_length:
+                # Update the result
+                res = s[l: r + 1]
+                # Update max length
+                max_length = r - l + 1
 
-            # 2. Take care even length palindromes
-            l, r = i, i + 1
-            # while l and r are in bound, and it's palindrome (l and r are equal)
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                # Check the length of the palindrome
-                if (r - l + 1) > max_length:
-                    # Update the result
-                    res = s[l: r + 1]
-                    # Update max mlength
-                    max_length = r - l + 1
-                # Expand pointers
-                l -= 1
-                r += 1
+            # setup pointers
+            l, r = expand_even_length(s, i, i + 1)
+            # Check the length of the palindrome
+            if (r - l + 1) > max_length:
+                # Update the result
+                res = s[l: r + 1]
+                # Update max length
+                max_length = r - l + 1
         return res
+
+# Helper function that checks odd length palindrome
+def expand_odd_length(s, l, r):
+    # while l and r are in bound, and it's palindrome (l and r are equal)
+    while l >= 0 and r < len(s) and s[l] == s[r]:
+        # Expand pointers
+        l -= 1
+        r += 1
+    return l + 1, r - 1
+
+# Helper function that checks even length palindrome
+def expand_even_length(s, l, r):
+    # while l and r are in bound, and it's palindrome (l and r are equal)
+    while l >= 0 and r < len(s) and s[l] == s[r]:
+        # Expand pointers
+        l -= 1
+        r += 1
+    return l + 1, r - 1
         
