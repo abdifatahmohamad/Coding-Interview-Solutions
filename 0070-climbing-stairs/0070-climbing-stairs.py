a28492835f3b6973dcf52033485a9807f4b10f81
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2023-02-12 15:26:29 -0600

    Time: 24 ms (95.39%), Space: 14 MB (7.79%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,8 +1,8 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        cache = [0] * (n + 1)
-        cache[0] = 1
-        cache[1] = 1
-        for i in range(2, n + 1):
-            cache[i] = cache[i - 1] + cache[i - 2]
-        return cache[n]
+        cache = [0 for _ in range(n + 1)]
+        cache[-1] = 1
+        cache[-2] = 1
+        for i in range(n - 2, -1, -1):
+            cache[i] = cache[i + 1] + cache[i + 2]
+        return cache[0]
