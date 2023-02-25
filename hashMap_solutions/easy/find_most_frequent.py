3ee38def454f8c167c1f9cfee864f9fef0a5406a
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2023-02-24 20:42:11 -0600

    Update find_most_frequent.py

--- hashMap_solutions/easy/find_most_frequent.py
+++ hashMap_solutions/easy/find_most_frequent.py
@@ -1,3 +1,4 @@
+
 # Find the most frequent from the nums.
 # If there is a tie, return all the values with the highest frequency in any order.
 nums = [2, 9, 1, 8, 4, 1, 9, 1, 4, 6, 2, 9, 2]
@@ -6,10 +7,14 @@ for n in nums:
     mapping[n] = mapping.get(n, 0) + 1
 
 
-max_freq = 0
-for n in mapping:
-    if mapping.get(n) > max_freq:
-        max_freq = mapping.get(n)
+# max_freq = 0
+# for n in mapping:
+#     if mapping.get(n) > max_freq:
+#         max_freq = mapping.get(n)
+
+# Shorter version
+max_freq = max(mapping.values())
+
 res = []
 for n in mapping:
     if mapping.get(n) == max_freq:
