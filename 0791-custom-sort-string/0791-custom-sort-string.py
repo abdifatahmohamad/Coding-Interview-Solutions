2f33a11b8ccf9115eb2c9d94a229366098be6552
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2023-05-15 10:26:16 -0500

    Time: 49 ms (13.05%), Space: 16.3 MB (19.89%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.py
+++ 0791-custom-sort-string/0791-custom-sort-string.py
@@ -4,20 +4,14 @@ class Solution:
         for c in s:
             mp[c] = mp.get(c, 0) + 1
 
-        # res = [0 for _ in range(len(order))] 
-        # res = [0] * len(order)
         res = []
-        # order_index = {ch: i for i, ch in enumerate(order)}
         for ch in order:
             if ch in mp:
-                # index = order_index[ch]
-                # res[index] = ch * mp[ch]
                 res.append(ch * mp[ch])
                 del mp[ch]
 
         for ch, freq in mp.items():
             res.append(ch * freq)
-        # resStr = str(res)
-
+            
         return "".join(res)
         
