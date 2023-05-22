b313d8440350945419f85fb9f13cae164ab7080b
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2023-05-22 13:48:12 -0500

    Time: 348 ms (6.52%), Space: 52.9 MB (34.78%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cs
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cs
@@ -1,15 +1,15 @@
 public class Solution {
+    
+    // O(N Log N) time complexity || O(1) space complexity
     public int MinPairSum(int[] nums) {
-        
+        // Sort array in ascending order
         Array.Sort(nums);
-        
-        int[] sortedArray = nums;
-        
-        int i = 0;
-        int j = sortedArray.Length - 1;
+
+        // Using two pointers technique get maximum between current sum and max pair sum.
+        int i = 0, j = nums.Length - 1;
         int maxSum = int.MinValue;
         while(i < j){
-            maxSum = Math.Max(maxSum, sortedArray[i] + sortedArray[j]);
+            maxSum = Math.Max(maxSum, nums[i] + nums[j]);
             i++;
             j--;
         }
