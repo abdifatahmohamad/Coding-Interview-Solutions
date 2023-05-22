354a30337985a7c3e25108616567c570ac8ba422
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2023-05-22 14:46:56 -0500

    Time: 188 ms (63.17%), Space: 50.8 MB (76.13%) - LeetHub

---
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int FindKthLargest(int[] nums, int k) {
+        var pq = new PriorityQueue<int, int>();
+
+        foreach(var num in nums)
+        {
+            pq.Enqueue(num, num);
+
+            if (pq.Count > k)
+            {
+                pq.Dequeue();
+            }
+        }
+
+        return pq.Peek();
+        
+    }
+}
