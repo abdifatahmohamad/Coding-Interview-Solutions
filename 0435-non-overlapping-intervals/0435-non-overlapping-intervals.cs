8ff40469d5efe0f3b5be5a377513b21dc0177d02
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2023-05-28 18:37:59 -0500

    Time: 419 ms (66.84%), Space: 73.3 MB (13.90%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cs
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cs
@@ -2,8 +2,9 @@ public class Solution {
     public int EraseOverlapIntervals(int[][] intervals) {
         int end = int.MinValue;
         int res = 0;
-        var points = intervals.OrderBy(x => x[1]);
-        foreach (int[] arr in points) {
+        // var points = intervals.OrderBy(x => x[1]);
+        Array.Sort(intervals, (x, y) => x[1].CompareTo(y[1]));
+        foreach (int[] arr in intervals){
             if (arr[0] >= end){
                 end = arr[1];
             } else{
