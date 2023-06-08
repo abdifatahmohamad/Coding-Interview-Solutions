73eebc84ad7d6e1f54ecebd2f60b8c9b6d3081db
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2023-06-07 20:15:33 -0500

    Time: 117 ms (5.53%), Space: 42.9 MB (47.93%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cs
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cs
@@ -1,46 +1,23 @@
 public class Solution {
-    public int CountNegatives(int[][] matrix) {
-        int rows = matrix.Length;
-        int cols = matrix[0].Length;
+    public int CountNegatives(int[][] grid) {
+        int m = grid.Length;
+        int n = grid[0].Length;
 
-        bool[,] visited = new bool[rows, cols];
-        int count = 0;
+        int i = m - 1, j = 0;
 
-        int[] dr = { -1, 1, 0, 0 };
-        int[] dc = { 0, 0, -1, 1 };
-
-        Queue<int[]> queue = new Queue<int[]>();
-
-        queue.Enqueue(new int[] { 0, 0 });  // Start from the top-left corner
-
-        while (queue.Count > 0)
-        {
-            int[] currPos = queue.Dequeue();
-            int r = currPos[0];
-            int c = currPos[1];
-
-            // Check if the current position is within the matrix boundaries
-            if (r >= 0 && r < rows && c >= 0 && c < cols && !visited[r, c])
-            {
-                visited[r, c] = true;
-
-                // Check if the current value is negative
-                if (matrix[r][c] < 0)
-                {
-                    count++;
-                }
-
-                // Enqueue the adjacent positions
-                for (int i = 0; i < 4; i++)
-                {
-                    int newRow = r + dr[i];
-                    int newCol = c + dc[i];
-                    queue.Enqueue(new int[] { newRow, newCol });
-                }
+        int res = 0;
+        while(i >= 0 && j < n){
+            // If we found a negative number, move up a row
+            if(grid[i][j] < 0){
+                res += n - j;
+                i--;
+            // Else, move right to see if we can find the negative number
+            } else{
+                j++;
             }
         }
-
-        return count;
+        
+        return res;
         
     }
 }
