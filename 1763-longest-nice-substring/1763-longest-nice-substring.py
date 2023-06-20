324b5d4ca87e24031bc984e77a65569ce91bfb32
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2023-06-19 22:40:47 -0500

    Time: 149 ms (34.48%), Space: 16.4 MB (15.52%) - LeetHub

--- 1763-longest-nice-substring/1763-longest-nice-substring.py
+++ 1763-longest-nice-substring/1763-longest-nice-substring.py
@@ -1,18 +1,5 @@
 class Solution:
     def longestNiceSubstring(self, s: str) -> str:
-        def is_nice(sub):
-            char_count = {}
-            for char in sub:
-                char_count[char] = char_count.get(char, 0) + 1
-
-            for char in char_count:
-                if char.islower() and char.upper() not in char_count:
-                    return False
-                if char.isupper() and char.lower() not in char_count:
-                    return False
-
-            return True
-    
         n = len(s)
         max_length = 0
         max_start = 0
@@ -22,13 +9,22 @@ class Solution:
             r = l + 1
             while r <= n:
                 substring = s[l:r]
-                if is_nice(substring) and len(substring) > max_length:
+                if self.is_nice(substring) and len(substring) > max_length:
                     max_length = len(substring)
                     max_start = l
                     max_end = r
                 r += 1
 
         return s[max_start:max_end]
+    
+    
+    # Helper method
+    def is_nice(self, s):
+        unique = set(s)
+        for ch in unique:
+            if ch.lower() not in unique or ch.upper() not in unique:
+                return False
+        return True
     
  
 
