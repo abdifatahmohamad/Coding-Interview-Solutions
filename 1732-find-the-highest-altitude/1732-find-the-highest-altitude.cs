240f1999962737bbf2bc03bdac8239c13e7dfa2b
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2023-06-22 20:45:35 -0500

    Time: 80 ms (87.95%), Space: 38.6 MB (20.38%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cs
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cs
@@ -1,16 +1,17 @@
 public class Solution {
     public int LargestAltitude(int[] gain) {
-        List<int> list = new List<int>();
-        list.Add(0);
-        int startedTrip = 0 + (gain[0]);
-        list.Add(startedTrip);
-        for(int i = 1; i < gain.Length; i++){
-            int altitude = startedTrip + (gain[i]);
-            startedTrip = altitude;
-            list.Add(altitude);
+        int max_altitude = int.MinValue;
+        int started_trip = 0 + gain[0];
+        max_altitude = Math.Max(started_trip, 0);
+
+        for (int i = 1; i < gain.Length; i++)
+        {
+            int alt = started_trip + gain[i];
+            started_trip = alt;
+            max_altitude = Math.Max(max_altitude, alt);
         }
-        
-        return list.Max();
+
+        return max_altitude;
         
     }
 }
