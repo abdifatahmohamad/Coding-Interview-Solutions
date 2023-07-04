29da2fe68a89c28fb7215a785085c892f31b13c8
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2023-07-04 18:52:08 -0500

    Time: 0 ms (100.00%), Space: 56.1 MB (59.15%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,6 +1,10 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        k %= nums.length;
+        // Handle the edge case where k is greater than the length of the array
+        if(k >= nums.length){
+            k %= nums.length;
+        }
+        
         reverse(nums, 0, nums.length-1);  // reverse the whole array
         reverse(nums, 0, k-1);  // reverse the first part
         reverse(nums, k, nums.length-1);  // reverse the second part
