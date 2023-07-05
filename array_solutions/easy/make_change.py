4deb7f58ef383689562588bf24657154518d82ed
Abdifatah Mohamed <mofaatah@gmail.com>
2023-07-04 21:04:41 -0500

    Added new solutions to leetcode solutions folder

--- array_solutions/easy/make_change.py
+++ array_solutions/easy/make_change.py
@@ -45,10 +45,78 @@ class Solution:
                 while n - denominations[i] >= 0:
                     res[i] += 1
                     n -= denominations[i]
-
         return res
 
 
 solution = Solution()
-print(solution.make_change(200))  # (3, 1, 0, 1)
+print(solution.make_change(33))  # (3, 1, 0, 1)
 print(solution.make_change(23))  # (3, 0, 2, 0)
+
+
+"""
+Explanation:
+
+    Let's say n = 33
+    dominations = [1, 5, 10, 25]
+    dominations[i] = 25
+    res = [0, 0, 0, 0]
+    n = 33
+    33 >= 25 == true
+        while 33 - 25 >= 0 == true
+            res[3] += 1
+            33 - 25 = 8
+    res = [0, 0, 0, 1]
+    
+    Iteration 2:
+    n = 8
+    dominations[i] = 10
+    8 >= 10 == false
+    while 8 - 10 >= 0 == false
+        the condition is false so nothing happens here
+        res[2] += 0
+    res = [0, 0, 0, 1]
+
+
+    Iteration 3:
+    n = 8
+    dominations[i] = 5
+    8 >= 5 == true
+    while 8 - 5 >= 0 == true
+        res[1] += 1
+        8 - 5 = 3
+    res = [0, 1, 0, 1]
+
+    Iteration 4:
+    n = 3
+    dominations[i] = 1
+    3 >= 1 == true
+    while 3 - 1 >= 0 == true
+        res[0] += 1
+        3 - 1 = 2
+    res = [1, 1, 0, 1]
+
+    n = 2
+    dominations[i] = 1
+    2 >= 1 == true
+    while 2 - 1 >= 0 == true
+        res[0] += 1
+        2 - 1 = 1
+    res = [2, 1, 0, 1]
+
+    n = 1
+    dominations[i] = 1
+    1 >= 1 == true
+    while 1 - 1 >= 0 == true
+        res[0] += 1
+        1 - 1 = 0
+    res = [3, 1, 0, 1]
+
+    n = 0
+    dominations[i] = 1
+    0 >= 1 == false
+    while 0 - 1 >= 0 == true
+        the condition is false so nothing happens here
+
+    return res = [3, 1, 0, 1]
+
+    """
