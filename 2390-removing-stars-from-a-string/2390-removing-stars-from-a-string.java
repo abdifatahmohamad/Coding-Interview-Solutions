28ad44cd3fe3ea93be1fd7f9c3a47eeb86bed6dd
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2023-07-06 20:24:24 -0500

    Time: 351 ms (26.07%), Space: 45.7 MB (39.38%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
@@ -4,18 +4,30 @@ class Solution {
     }
     
     // Helper method that removes string #
-    private String processString(String s){
-        StringBuilder sb = new StringBuilder();
-        for(char ch : s.toCharArray()){
-            if(ch == '*'){
-                if(sb.length() > 0){
-                    sb.deleteCharAt(sb.length() - 1);
+    private String processString(String s) {
+        // Create a stack to store characters
+        Stack<Character> stack = new Stack<>();
+        
+        // Iterate over each character in the string
+        for (char ch : s.toCharArray()) {
+            if (ch == '*') {
+                // Handle backspace: if stack is not empty, remove the topmost character
+                if (!stack.isEmpty()) {
+                    stack.pop();
                 }
-            } else{
-                sb.append(ch);
+            } else {
+                // Add non-backspace character to the stack
+                stack.push(ch);
             }
         }
         
+        // Build the final string from the characters in the stack
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.insert(0, stack.pop());
+        }
+        
+        // Return the processed string
         return sb.toString();
     }
 }
