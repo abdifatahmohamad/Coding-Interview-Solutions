8cc1a1f96f03ad30906f47e42048a537a2fd5656
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2023-07-06 20:14:31 -0500

    Time: 1 ms (83.22%), Space: 40.8 MB (62.45%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -1,45 +1,23 @@
 class Solution {
-    public boolean backspaceCompare(String s, String t) {   
-        int i = s.length() - 1; // Initialize pointer i to the last character of string S
-        int j = t.length() - 1; // Initialize pointer j to the last character of string T
-
-        while (i >= 0 || j >= 0) { // Iterate until either pointer i or j reaches the beginning of their respective strings
-            i = processString(s, i); // Process string s and update pointer i
-            j = processString(t, j); // Process string t and update pointer j
-
-            if (i >= 0 && j >= 0 && s.charAt(i) != t.charAt(j)) {
-                // If both strings have non-skipped characters at the same index and they are not equal, return false
-                return false;
-            }
-
-            if ((i >= 0) != (j >= 0)) {
-                // If one string has more non-skipped characters than the other, return false
-                return false;
-            }
-
-            i--; // Decrement pointer i
-            j--; // Decrement pointer j
-        }
-
-        return true;       
+    public boolean backspaceCompare(String s, String t) {
+        return processString(s).equals(processString(t));
     }
     
-    // Helper method that applies the backspace rules to a given string and returns the index of the last          non-skipped character.
-    private int processString(String str, int index) {
-        int skip = 0; // Counter for backspace characters encountered
-
-        while (index >= 0) {
-            if (str.charAt(index) == '#') { // If current character is a backspace character
-                skip++; // Increment backspace counter
-                index--; // Move to the previous character
-            } else if (skip > 0) { // If backspace counter is positive
-                skip--; // Decrement backspace counter
-                index--; // Move to the previous character
-            } else { // If current character is a non-backspace character and no backspaces need to be skipped
-                break; // Break the loop
+    // Helper method that removes string #
+    private String processString(String s){
+        StringBuilder sb = new StringBuilder();
+        for(char ch : s.toCharArray()){
+            if(ch == '#'){
+                if(sb.length() > 0){
+                    sb.deleteCharAt(sb.length() - 1);
+                }
+            } else{
+                sb.append(ch);
             }
         }
-
-        return index; // Return the updated index after processing the string
+        
+        return sb.toString();
     }
+    
+    
 }
