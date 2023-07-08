6af581731d09a372841c7d55d337e9842f202394
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2023-07-07 22:10:43 -0500

    Time: 0 ms (100.00%), Space: 40.5 MB (87.52%) - LeetHub

---
+++ 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int countElements(int[] nums) {
+        
+        // Find the minimum and maximum elements in the array
+        int minVal = Integer.MAX_VALUE;
+        int maxVal = Integer.MIN_VALUE;
+        for (int n : nums) {
+            minVal = Math.min(minVal, n);
+            maxVal = Math.max(maxVal, n);
+        }
+
+        int res = 0;
+
+        // Count elements with both strictly smaller and strictly greater elements
+        for (int n : nums) {
+            // Check if the element has both strictly smaller and strictly greater elements
+            if (n > minVal && n < maxVal)  
+                res++;
+        }
+
+        return res;  // Return the final count
+    }
+}
