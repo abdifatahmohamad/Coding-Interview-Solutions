2c7e49c3c50576287cd43b63001080c5328cc850
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2023-07-12 00:08:27 -0500

    Time: 4 ms (88.28%), Space: 85.1 MB (5.53%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,6 +1,28 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
         int n = nums.length;
+        int[] res = new int[n];
+        int i = 0; // positive index
+        int j = 1; // negative index
+
+        // Separate positive and negative numbers into separate arrays 
+        // while populating the result array
+        for (int num : nums) {
+            if (num < 0) {
+                res[j] = num;
+                j += 2;
+            } else {
+                res[i] = num;
+                i += 2;
+            }
+        }
+
+        return res;
+    }
+    
+    // 
+    public int[] rearrangeArrayMine(int[] nums) {
+        int n = nums.length;
         int[] pos = new int[n / 2];
         int[] neg = new int[n / 2];
         
@@ -15,24 +37,22 @@ class Solution {
                 pos[posIndex++] = nums[i];
             }
         }
-
-        int[] res = new int[n];
         int idx = 0;
         for (int i = 0; i < n / 2; i++) {
-            res[idx++] = pos[i];
-            res[idx++] = neg[i];
+            nums[idx++] = pos[i];
+            nums[idx++] = neg[i];
         }
 
         // If there are any remaining elements in either positive or negative array, add them to the result
         if (posIndex < n / 2) {
-            res[idx++] = pos[idx];
+            nums[idx++] = pos[idx];
         }
 
         if (negIndex < n / 2) {
-            res[idx++] = neg[idx];
+            nums[idx++] = neg[idx];
         }
 
-    return res;
+    return nums;
         
     }
 }
