7ad2a39863d6554432c0dacafd59e51ea344d7c3
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2023-07-11 23:24:15 -0500

    Time: 4 ms (52.10%), Space: 43.9 MB (38.94%) - LeetHub

--- 2164-sort-even-and-odd-indices-independently/2164-sort-even-and-odd-indices-independently.java
+++ 2164-sort-even-and-odd-indices-independently/2164-sort-even-and-odd-indices-independently.java
@@ -1,5 +1,28 @@
 class Solution {
+    // Sugal code
     public int[] sortEvenOdd(int[] nums) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
+        for (int i = 0; i < nums.length; i++) {
+            (i % 2 == 0 ? minHeap : maxHeap).offer(nums[i]);
+        }
+        
+        int i = 0;
+        while (!minHeap.isEmpty() || !maxHeap.isEmpty()) {
+            if (!minHeap.isEmpty()) {
+                nums[i++] = minHeap.poll();
+            }
+            if (!maxHeap.isEmpty()) {
+                nums[i++] = maxHeap.poll();
+            }
+        }
+        return nums;
+    }
+    
+    
+    
+    // My solution
+    public int[] sortEvenOddMin(int[] nums) {
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
 
@@ -18,9 +41,11 @@ class Solution {
             res[i++] = maxHeap.poll();
         }
         
+        // Left over minHeap
         while (!minHeap.isEmpty()) {
             res[i++] = minHeap.poll();
         }
+        // Left over maxHeap
         while (!maxHeap.isEmpty()) {
             res[i++] = maxHeap.poll();
         }
