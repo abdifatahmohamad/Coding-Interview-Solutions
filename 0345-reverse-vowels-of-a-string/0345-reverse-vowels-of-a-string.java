ca2a0741031284cb750d32746ae1e0730faa01f1
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2023-07-13 18:25:43 -0500

    Time: 6 ms (59.25%), Space: 44.2 MB (40.68%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,41 +1,49 @@
 class Solution {
     public String reverseVowels(String s) {
+        // Convert the input string to a character array
         char[] chars = s.toCharArray();
         int start = 0;
         int end = chars.length - 1;
-
-        reverseVowelsInRange(chars, start, end);
-
+        reverseVowels(chars, start, end);
+        // Convert the modified character array back to a string
         return new String(chars);
         
     }
     
-    private static void reverseVowelsInRange(char[] chars, int start, int end) {
+    // Helper method that reverses vowels
+    private static void reverseVowels(char[] chars, int start, int end) {
+        // Array of vowel characters
         char[] vowels = {'a', 'e', 'i', 'o', 'u'};
-
+        // Iterate until the start pointer crosses the end pointer
         while (start < end) {
             if (isVowel(chars[start], vowels) && isVowel(chars[end], vowels)) {
-                // Swap the vowels
+                // If both the start and end characters are vowels, swap them
                 char temp = chars[start];
                 chars[start] = chars[end];
                 chars[end] = temp;
                 start++;
                 end--;
             } else if (isVowel(chars[start], vowels)) {
+                // If the start character is a vowel and the end character is not, 
+                // move the end pointer towards the center
                 end--;
             } else {
+                // If the start character is not a vowel, 
+                // move the start pointer towards the center
                 start++;
             }
         }
     }
     
     private static boolean isVowel(char c, char[] vowels) {
+         // Convert the character to lowercase
         c = Character.toLowerCase(c);
         for (char vowel : vowels) {
             if (vowel == c) {
-                return true;
+                // If the character matches a vowel in the array, return true
+                return true; 
             }
         }
-        return false;
+        return false; 
     }
 }
