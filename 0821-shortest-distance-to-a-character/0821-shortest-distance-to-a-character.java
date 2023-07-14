23d8a34bc91e009475eee43113ca9d854c148f61
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2023-07-13 20:11:00 -0500

    Time: 1 ms (97.37%), Space: 42 MB (27.19%) - LeetHub

--- 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.java
+++ 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.java
@@ -1,36 +1,50 @@
 class Solution {
     public int[] shortestToChar(String s, char c) {
         char[] chars = s.toCharArray();
-        int[] indices = new int[chars.length];
-        int count = 0;
-        // Iterate over the char array and store the indices
-        for (int i = 0; i < chars.length; i++) {
-            if (chars[i] == c) {
-                indices[count] = i;
-                count++;
-            }
-        }
         int[] res = new int[s.length()];
-        // Find the closest occurrence of c for index 0
-        for (int i = 0; i < chars.length; i++) {
-            res[i] = findClosestOccurrence(i, indices, count);
-        }
-        return res;
-        
-    }   
-    // Helper method to find the closest occurrence of 'e' for an index
-    private static int findClosestOccurrence(int index, int[] indices, int count) {
-        int minDistance = Integer.MAX_VALUE;
-
-        for (int j = 0; j < count; j++) {
-            int distance = Math.abs(index - indices[j]);
-
-            if (distance < minDistance) {
-                minDistance = distance;
+        // Find the closest occurrence of c for each index 0
+        int left = -1;
+        int right = -1;
+        int i = 0;
+        while (i < chars.length) {
+            if (chars[i] == c) {
+                // Update the left pointer if the current character is the target
+                left = i; 
             }
-        }
 
-        return minDistance;
+            if (left != -1) {
+                // Calculate the distance to the left pointer if it has been set
+                res[i] = i - left; 
+            }
+
+            i++;
+        }
+        
+        // Reset the loop counter to the last index
+        i = chars.length - 1; 
+
+        while (i >= 0) {
+            if (chars[i] == c) {
+                // Update the right pointer if the current character is the target
+                right = i; 
+            }
+
+            if (right != -1) {
+                if (res[i] == 0) {
+                    // Calculate the distance to the right pointer 
+                    // if it has been set and no distance has been calculated yet
+                    res[i] = right - i; 
+                } else {
+                    // Update the distance to the right pointer 
+                    // if it is smaller than the current distance
+                    res[i] = Math.min(res[i], right - i); 
+                }
+            }
+
+            i--;
+        }
+        
+        return res;
     }
 }
 
