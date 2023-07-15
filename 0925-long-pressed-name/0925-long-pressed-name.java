7ad36ca89166b4776483ea5b661370639225d01b
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2023-07-14 20:39:23 -0500

    Time: 1 ms (71.50%), Space: 40.6 MB (55.19%) - LeetHub

--- 0925-long-pressed-name/0925-long-pressed-name.java
+++ 0925-long-pressed-name/0925-long-pressed-name.java
@@ -1,22 +1,45 @@
 class Solution {
     public boolean isLongPressedName(String name, String typed) {
-        int i = 0; // Pointer for the name string
-        int j = 0; // Pointer for the typed string
-
-        while (j < typed.length()) {
-            if (i < name.length() && name.charAt(i) == typed.charAt(j)) {
-                // Characters match, move both pointers
+        // Case1: handle if name length > typed length 
+        if (name.length() > typed.length()) {
+            return false;
+        }
+        int i = 0, j = 0;
+        // Keeping looping until we have processed all characters in either name or typed
+        while (i < name.length() || j < typed.length()) {
+            // Check if we are within the bounds of both name and typed,
+            // and if the characters at the current positions i and j match
+            if (i < name.length() && j < typed.length() && name.charAt(i) == typed.charAt(j)) {
                 i++;
                 j++;
-            } else if (j > 0 && typed.charAt(j) == typed.charAt(j - 1)) {
-                // Long-pressed character, move the pointer in the typed string
-                j++;
+            } else if (j > 0 && j < typed.length() && typed.charAt(j) == typed.charAt(j - 1)) {
+                // Skip repeated characters in typed
+                j++; 
             } else {
-                // Mismatch
-                return false;
+                // Mismatched characters
+                return false; 
             }
         }
-
-        return i == name.length();
+        
+        return true;
     }
 }
+
+/*
+    - Order is matter
+    - typed length always >= name length
+    - All characters in typed is in name
+    
+    Case1: If name length > typed length : return false
+    Case2: If string name != string typed: return false
+    Case3: if first character of typed != first character of typed: return false
+    
+    Test cases:
+
+    1. name = "alex", typed = "aaleex" : return true
+    2. name = "saeed", typed = "ssaaedd" : return false
+    3. name = "rick", typed = "kric": return false
+    4. name = "xnhtq", typed = "xhhttqq": return false
+    5. name = "alex", typed = "aaleexa": return false
+
+*/
