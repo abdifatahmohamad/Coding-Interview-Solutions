db18efbeece0c74758e88565002010b435a8b955
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2023-07-16 02:46:48 -0500

    Time: 8 ms (5.22%), Space: 40.4 MB (29.92%) - LeetHub

---
+++ 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean haveConflict(String[] event1, String[] event2) {
+        // Convert event strings to integer arrays [startTime, endTime]
+        int[] event1Times = convertToMinutes(event1);
+        int[] event2Times = convertToMinutes(event2);
+                System.out.println("Event1: start= " + event2Times[0] + ", end= " + event2Times[1]);
+
+        // Check if there is any intersection between the two events
+        if (event1Times[0] <= event2Times[1] && event2Times[0] <= event1Times[1]) {
+            // Intersection exists, return true
+            return true;
+        } else {
+            // No intersection, return false
+            return false;
+        }
+    }
+
+    private int[] convertToMinutes(String[] event) {
+        int[] times = new int[2];
+        for (int i = 0; i < 2; i++) {
+            String[] parts = event[i].split(":");
+            int hours = Integer.parseInt(parts[0]);
+            int minutes = Integer.parseInt(parts[1]);
+            times[i] = hours * 60 + minutes;
+        }
+        return times;
+    }
+}
