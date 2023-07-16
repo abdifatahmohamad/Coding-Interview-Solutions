660ac593fbb2eda81b00f2978d3da5cdf34d3a68
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2023-07-16 02:50:56 -0500

    Time: 8 ms (5.22%), Space: 41 MB (5.82%) - LeetHub

--- 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.java
+++ 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.java
@@ -3,7 +3,7 @@ class Solution {
         // Convert event strings to integer arrays [startTime, endTime]
         int[] event1Times = convertToMinutes(event1);
         int[] event2Times = convertToMinutes(event2);
-                System.out.println("Event1: start= " + event2Times[0] + ", end= " + event2Times[1]);
+        System.out.println("Event1: start= " + event2Times[0] + ", end= " + event2Times[1]);
 
         // Check if there is any intersection between the two events
         if (event1Times[0] <= event2Times[1] && event2Times[0] <= event1Times[1]) {
@@ -18,11 +18,15 @@ class Solution {
     private int[] convertToMinutes(String[] event) {
         int[] times = new int[2];
         for (int i = 0; i < 2; i++) {
-            String[] parts = event[i].split(":");
-            int hours = Integer.parseInt(parts[0]);
-            int minutes = Integer.parseInt(parts[1]);
-            times[i] = hours * 60 + minutes;
+            // Split the time string into hours and minutes
+            String[] parts = event[i].split(":"); 
+             // Extract the hours part and convert it to an integer
+            int hours = Integer.parseInt(parts[0]); 
+            // Extract the minutes part and convert it to an integer
+            int minutes = Integer.parseInt(parts[1]);  
+            // Convert hours to minutes and add them to the minutes
+            times[i] = hours * 60 + minutes;  
         }
-        return times;
+        return times;  // Return the array [startTime, endTime]
     }
 }
