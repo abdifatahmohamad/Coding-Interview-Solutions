b0a8fed977758ef9eda24bb0ced01dafea050400
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2023-07-16 02:04:01 -0500

    Time: 5 ms (30.01%), Space: 43.1 MB (6.53%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
@@ -1,20 +1,27 @@
 class Solution {
     public boolean judgeSquareSum(int c) {
+         // Calculate the square root of c
+        int cSqr = (int) Math.sqrt(c);
+        // Create an array to store numbers from 0 to cSqr
+        int[] nums = new int[cSqr + 1];
+        // Fill the nums array with numbers from 0 to cSqr
+        for (int i = 0; i <= cSqr; i++) {
+            nums[i] = i;
+        }
+
         int left = 0;
-        // Initialize the right pointer to the square root of c
-        int right = (int) Math.sqrt(c); 
-        // Continue looping until left becomes greater than right
-        while (left <= right) { 
-            // Calculate the sum of squares of left and right
-            // Use long data type for sum calculation
-            long sum = (long) left * left + (long) right * right; 
-            if (sum == c) { 
-                return true;
+        int right = cSqr;
+        while (left <= right) {
+            // Calculate the sum of squares
+            long sum = (long) nums[left] * nums[left] + (long) nums[right] * nums[right];
+            
+            // Check if the sum is equal to c
+            if (sum == c) {
+                return true; // Two numbers found whose squares sum up to c
             } else if (sum < c) {
-                // Increment the left pointer to consider a larger value
-                left++; 
+                left++; // Increment the left pointer to consider a larger number
             } else {
-                right--;
+                right--; // Decrement the right pointer to consider a smaller number
             }
         }
 
