1967d8f4061ecd78e1c489c06d0d0926514091df
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2023-07-16 16:35:42 -0500

    Time: 8 ms (53.79%), Space: 41.6 MB (35.05%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.java
+++ 0763-partition-labels/0763-partition-labels.java
@@ -1,20 +1,17 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
         char[] chars = s.toCharArray();
-        Map<String, Integer> map = new HashMap<>();
+        Map<Character, Integer> map = new HashMap<>();
         for (int i = 0; i < chars.length; i++) {
-            map.put(String.valueOf(chars[i]), i);
-            // map.put(chars[i] + "", i);
+            map.put(chars[i], i);
         }
         
         List<Integer> res = new ArrayList<>();
         int size = 0;
         int end = 0;
         for (int i = 0; i < s.length(); i++) {
-            // Retrieve character from s
-            char ch = s.charAt(i); 
             // Retrieve key from map
-            String key = String.valueOf(ch); 
+            Character key = s.charAt(i);
             // Retrieve last index from map
             int lastIndex = map.get(key); 
             // As soon as we see a character, update the size of partition
