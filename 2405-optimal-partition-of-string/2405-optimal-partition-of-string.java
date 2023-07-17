b3461ed2804ffe3066a84fe4dbd7c7089d3f3136
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2023-07-16 19:04:51 -0500

    Time: 36 ms (67.11%), Space: 44.6 MB (24.37%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
@@ -1,18 +1,18 @@
 class Solution {
     public int partitionString(String s) {
-        Map<Character, Integer> seen = new HashMap<>();
+        Set<Character> seen = new HashSet();
         // Start result at 1 since we are guarantee that string will not be an empty 
         int res = 1;
         for (char c : s.toCharArray()) {
             // If character in map
-            if(seen.containsKey(c)){
+            if(seen.contains(c)){
                 res++;
                 // Empty the map
                 seen.clear();
             } // If chacarter not in map
             
             // Put that character in the map
-            seen.put(c, seen.getOrDefault(c, 0));
+            seen.add(c);
         
         }  
         return res;
