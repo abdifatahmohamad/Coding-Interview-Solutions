6d74b671ffa5d62c111091a42c04c654981b09d3
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2023-07-18 20:11:22 -0500

    Time: 4 ms (80.09%), Space: 63.8 MB (38.24%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
@@ -10,6 +10,7 @@
  */
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
+        // Check if head is null or has no next node
         if (head == null || head.next == null) {
             return null; // No middle node to delete
         }
@@ -18,18 +19,20 @@ class Solution {
         ListNode fast = head;
         ListNode prev = null;
 
+        // Move the pointers until the fast pointer reaches the end of the list
         while (fast != null && fast.next != null) {
-            fast = fast.next.next;
-            prev = slow;
-            slow = slow.next;
+            fast = fast.next.next; // Move fast pointer by two nodes
+            prev = slow; // Update previous pointer to be one step behind slow pointer
+            slow = slow.next; // Move slow pointer by one node
         }
 
+        // Remove the middle node by adjusting the previous pointer
         if (prev != null) {
-            prev.next = slow.next; // Remove the middle node
+            prev.next = slow.next;
         } else {
             head = slow.next; // Update head if the middle node is the head
         }
 
-        return head;
+        return head; // Return the modified head of the linked list
     }
 }
