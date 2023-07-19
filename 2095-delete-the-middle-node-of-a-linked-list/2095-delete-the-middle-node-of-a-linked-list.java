3ee7748fdc43ad577e0fce12c57f98443be32a26
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2023-07-18 20:12:32 -0500

    Time: 6 ms (11.86%), Space: 63.8 MB (55.08%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
@@ -10,29 +10,41 @@
  */
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
-        // Check if head is null or has no next node
         if (head == null || head.next == null) {
             return null; // No middle node to delete
         }
 
-        ListNode slow = head;
-        ListNode fast = head;
-        ListNode prev = null;
+        ListNode middle = findMiddleNode(head);
 
-        // Move the pointers until the fast pointer reaches the end of the list
-        while (fast != null && fast.next != null) {
-            fast = fast.next.next; // Move fast pointer by two nodes
-            prev = slow; // Update previous pointer to be one step behind slow pointer
-            slow = slow.next; // Move slow pointer by one node
+        // If the middle node is the head, update the head to the next node
+        if (head == middle) {
+            head = head.next;
+            return head;
+        }
+
+        // Find the node before the middle node
+        ListNode prev = head;
+        while (prev.next != middle) {
+            prev = prev.next;
         }
 
         // Remove the middle node by adjusting the previous pointer
-        if (prev != null) {
-            prev.next = slow.next;
-        } else {
-            head = slow.next; // Update head if the middle node is the head
-        }
+        prev.next = middle.next;
 
         return head; // Return the modified head of the linked list
+        
+    }
+    
+    // Method to find the middle node of the linked list
+    private ListNode findMiddleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        return slow;
     }
 }
