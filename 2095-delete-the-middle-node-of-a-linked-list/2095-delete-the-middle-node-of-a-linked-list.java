1bf36047e02a7aae46c4de59202b5518692f5317
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2023-07-18 20:08:15 -0500

    Time: 4 ms (80.14%), Space: 63.4 MB (94.45%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
@@ -10,37 +10,26 @@
  */
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
-        // Convert linked list to array
-        List<Integer> list = linkedListToArray(head);
-        // int mid = list.get(list.size() / 2);
-        int n = list.size();
-        int midIndex = n / 2;
-        
-        // Remove the middle element from the list
-        list.remove(midIndex);
-        // Convert the modified list back to a linked list
-        return arrayToLinkedList(list);
-    }
-    
-     // Helper method that converts linkedlist into an array
-    private List<Integer> linkedListToArray(ListNode head) {
-        List<Integer> list = new ArrayList<>();
-        ListNode curr = head;
-        while (curr != null) {
-            list.add(curr.val);
-            curr = curr.next;
+        if (head == null || head.next == null) {
+            return null; // No middle node to delete
         }
-        return list;
-    }
-    
-    // Helper method that converts an array to a linked list
-    private ListNode arrayToLinkedList(List<Integer> list) {
-        ListNode dummy = new ListNode(-1);
-        ListNode curr = dummy;
-        for (int num : list) {
-            curr.next = new ListNode(num);
-            curr = curr.next;
+
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            prev = slow;
+            slow = slow.next;
         }
-        return dummy.next;
+
+        if (prev != null) {
+            prev.next = slow.next; // Remove the middle node
+        } else {
+            head = slow.next; // Update head if the middle node is the head
+        }
+
+        return head;
     }
 }
