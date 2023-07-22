4bc921e59247b8ad7827cd7674376722120285c7
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2023-07-21 21:50:34 -0500

    Time: 0 ms (100.00%), Space: 40.9 MB (90.72%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,32 +1,19 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
         int left = 0;
-        int right = 1;
-        while(right < nums.length){
-            if(nums[left] != val){
-                left++;
-                right++;
-            } else if(nums[right] == val){
-                right++;
-            }else{
-                swab(nums, left, right);
-            }       
-        }
-        
+        int right = 0;
         int res = 0;
-        for(int n : nums){
-            if(n != val){
+        while(right < nums.length){
+            if(nums[right] != val){
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
                 res++;
             }
+            
+            right++;
         }
-        
-        return res;
-    }
-    
-    // Helper method that swabs two values
-    private void swab(int[] nums, int left, int right){
-        int temp = nums[left];
-        nums[left] = nums[right];
-        nums[right] = temp;
+        return res;    
     }
 }
