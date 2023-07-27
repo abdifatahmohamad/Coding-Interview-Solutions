7cc7ff011ca3c4a342700cfe3c3472d1b202acf3
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2023-07-26 20:16:23 -0500

    Time: 7 ms (81.80%), Space: 47.3 MB (35.64%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,37 +1,33 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+        // Created result array or array
         List<List<String>> res = new ArrayList<>();
-        // Create a map to store sorted strings as keys and their original strings as values
+        // Create map to fasilitate the operations
         Map<String, List<String>> map = new HashMap<>();
-
-        // Loop through each string in the array
-        for (String word : strs) {
-            // Sort the characters of the string and create a sorted key
+        // Loop the give array string
+        for(String word : strs){
+            // Convert each word into character array
             char[] chars = word.toCharArray();
+            // Sort characters
             Arrays.sort(chars);
-            String sortedStr = new String(chars);
-
+            // Convert character arrays back to string
+            // String sortedStr = new String(chars);
+            String sortedStr = String.valueOf(chars);
             // Check if the key already exists in the map
-            if (!map.containsKey(sortedStr)) {
-                // If not, create a new list and put the original string in it
+            if(!map.containsKey(sortedStr)){
+               // If not, create a new list and put the original string in it
                 List<String> newList = new ArrayList<>();
                 newList.add(word);
                 map.put(sortedStr, newList);
-            } else {
-                // If the key already exists, add the original string to the existing list
+            }else{
+             // If the key already exists, add the original string to the existing list
                 map.get(sortedStr).add(word);
             }
         }
-        
-        // Print map keys and values
-//         for(String key : map.keySet()){
-//             List<String> values = map.get(key);
-//             System.out.println(key + ":" + values);
-//         }
-        
-        for (List<String> words : map.values()) {
+        // Append map values to the result
+        for(List<String> words : map.values()){
             res.add(words);
-        } 
-        return res;    
+        }
+        return res;
     }
 }
