06d668bd445ac86573426d0609b2b91a30819643
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2023-07-27 19:52:03 -0500

    Time: 2 ms (97.46%), Space: 41.5 MB (53.08%) - LeetHub

--- 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.java
+++ 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.java
@@ -1,26 +1,20 @@
 class Solution {
-    Map<Character, Integer> map;
     public int canBeTypedWords(String text, String brokenLetters) {
-        map = new HashMap<>();
+        Map<Character, Integer> map = new HashMap<>();
         for(char c : brokenLetters.toCharArray()){
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
         
         int res = 0;
         for(String word : text.split(" ")){
-            if(canBeFullyTyped(word)){
-                res++;
-            }  
+            res++;
+            for(char c : word.toCharArray()){
+                if(map.containsKey(c)){
+                    res--;
+                    break;
+                }
+            }           
         }
         return res;
-    }
-    
-    private boolean canBeFullyTyped(String word){
-        for(char c : word.toCharArray()){
-            if(map.containsKey(c)){
-                return false;
-            }
-        }
-        return true;
     }
 }
