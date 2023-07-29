73de778d0e0ed5ad3f7bc2790fddd5ee181b84eb
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2023-07-28 21:32:58 -0500

    Time: 3 ms (72.56%), Space: 44 MB (25.15%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,33 +1,35 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        Map<Integer, Set<Character>> rows = new HashMap<>();
-        Map<Integer, Set<Character>> cols = new HashMap<>();
+        int rows = board.length;
+        int cols = board[0].length;
+        Map<Integer, Set<Character>> rowsMap = new HashMap<>();
+        Map<Integer, Set<Character>> colsMap = new HashMap<>();
         Map<Integer, Set<Character>> squares = new HashMap<>();
 
-        for (int r = 0; r < 9; r++) {
-            for (int c = 0; c < 9; c++) {
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
                 char value = board[r][c];
                 if (value != '.') {
                     // Check and create a new set if needed for rows
-                    if (!rows.containsKey(r))
-                        rows.put(r, new HashSet<>());
+                    if (!rowsMap.containsKey(r))
+                        rowsMap.put(r, new HashSet<>());
                     // Check and create a new set if needed for columns
-                    if (!cols.containsKey(c))
-                        cols.put(c, new HashSet<>());
+                    if (!colsMap.containsKey(c))
+                        colsMap.put(c, new HashSet<>());
                     // Check and create a new set if needed for squares
                     int squareIndex = 3 * (r / 3) + c / 3;
                     if (!squares.containsKey(squareIndex))
                         squares.put(squareIndex, new HashSet<>());
 
                     // Check if the value is already present in rows, columns, or squares
-                    if (rows.get(r).contains(value) || 
-                        cols.get(c).contains(value) ||
+                    if (rowsMap.get(r).contains(value) || 
+                        colsMap.get(c).contains(value) ||
                         squares.get(squareIndex).contains(value))
                         return false;
 
                     // Add the value to the respective row, column, and square
-                    rows.get(r).add(value);
-                    cols.get(c).add(value);
+                    rowsMap.get(r).add(value);
+                    colsMap.get(c).add(value);
                     squares.get(squareIndex).add(value);
                 }
             }
