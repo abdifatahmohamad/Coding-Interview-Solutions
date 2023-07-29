6ee67f520cab0a598031bb84be75e6c4c9890b76
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2023-07-28 20:06:55 -0500

    Time: 5 ms (99.81%), Space: 47 MB (58.34%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -5,23 +5,13 @@ class Solution {
         map = new HashMap<>();
 
         for (String word : strs) {
-            charFreq = new int[26];
-            for (char c : word.toCharArray()) {
-                charFreq[c - 'a']++;
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String sortedStr = String.valueOf(chars);
+            if (!map.containsKey(sortedStr)) {
+                map.put(sortedStr, new ArrayList<>());
             }
-
-            StringBuilder sb = new StringBuilder();
-            for (int count : charFreq) {
-                sb.append(count).append('#');
-            }
-
-            String key = sb.toString();
-            // System.out.println(key);
-
-            if (!map.containsKey(key)) {
-                map.put(key, new ArrayList<>());
-            }
-            map.get(key).add(word);
+            map.get(sortedStr).add(word);
         }
         
         return new ArrayList<>(map.values());
