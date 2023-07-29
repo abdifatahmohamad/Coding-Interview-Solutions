1afe74de423f908ab91dd7baf5f0681e5ba7e432
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2023-07-29 15:45:07 -0500

    Time: 44 ms (78.68%), Space: 55.2 MB (16.46%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -6,16 +6,22 @@ class Solution {
         while(right < fruits.length){
             int fRight = fruits[right];
             map.put(fRight, map.getOrDefault(fRight, 0) + 1);
+            // Validate if window is valid
             if(map.size() > 2){
                 int fLeft = fruits[left];
                 if(map.get(fLeft) == 1){
+                    // remove the left fruit from the map
                     map.remove(fLeft);
                 }else{
+                    // Decrement the left fruit
                     map.put(fLeft, map.get(fLeft) - 1);
                 }
+                // Incerement our left pointer
                 left++;
-            }
+            }   
+            // Maximize the todal number of fruits you can pick
             maxFruit = Math.max(maxFruit, right - left + 1);
+            // Incerement our right pointer
             right++;
         }
         return maxFruit;
