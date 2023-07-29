c82ef8549d508e4a036f30a5fdf31dcb19e2faa0
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2023-07-28 20:23:34 -0500

    Time: 14 ms (73.26%), Space: 48 MB (18.70%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,48 +1,26 @@
 class Solution {
-    // Min heap
+    Map<Integer, Integer> map;
+    PriorityQueue<Integer> minHeap;
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for(Integer n : nums){
+        map = new HashMap<>();
+        for(int n : nums){
             map.put(n, map.getOrDefault(n, 0) + 1);
         }
-        // Build priority queue based on lowest to highest frequency (Min heap)       
-        PriorityQueue<Integer> pq = new PriorityQueue((a, b) -> map.get(a) - map.get(b));
+        minHeap = new PriorityQueue<>((a, b) -> map.get(a) - map.get(b));
         for(int key : map.keySet()){
-            pq.offer(key);
-            if (pq.size() > k) {
-                pq.poll(); // Remove elements if the size exceeds k
+            minHeap.offer(key);
+            if(minHeap.size() > k){
+                minHeap.poll();
             }
         }
-        int[] res = new int[k];
-        int i = k - 1; // Start from the end of the array
-        while(i >= 0 && !pq.isEmpty()){
-            res[i--] = pq.poll();        
-        }
         
-        return res;     
-    }
-    
-    // Max heap
-    public int[] topKFrequentMaxHeap(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for(Integer n : nums){
-            map.put(n, map.getOrDefault(n, 0) + 1);
-        }
-        // Build priority queue based on highest to lowest frequency (Max heap)        
-        PriorityQueue<Integer> pq = new PriorityQueue((a, b) -> map.get(b) - map.get(a));
-        for(int key : map.keySet()){
-            pq.offer(key);
-        }
         int[] res = new int[k];
         int i = 0;
         while(k > i){
-            if(!pq.isEmpty()){
-                res[i++] = pq.poll();
-            }
+            if(!minHeap.isEmpty()){
+               res[i++] = minHeap.poll(); 
+            } 
         }
-        
-        return res;     
+        return res;
     }
-    
-    
 }
