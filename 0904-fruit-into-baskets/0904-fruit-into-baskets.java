1983a2d56d796bc98b16037d767db95a4526714e
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2023-07-29 15:51:58 -0500

    Time: 52 ms (74.74%), Space: 55.1 MB (26.64%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,28 +1,30 @@
 class Solution {
     public int totalFruit(int[] fruits) {
         Map<Integer, Integer> map = new HashMap<>();
-        int left = 0, right = 0;
+        int left = 0;
         int maxFruit = Integer.MIN_VALUE;
-        while(right < fruits.length){
+        int totalFruits = 0;
+        for(int right = 0; right < fruits.length; right++){
             int fRight = fruits[right];
             map.put(fRight, map.getOrDefault(fRight, 0) + 1);
-            // Validate if window is valid
+            totalFruits++;
+            // Validate the widnow
             if(map.size() > 2){
                 int fLeft = fruits[left];
-                if(map.get(fLeft) == 1){
-                    // remove the left fruit from the map
+                // Decrement left fruit from the hashmap
+                map.put(fLeft, map.get(fLeft) - 1);
+                // Decrement total fruits
+                totalFruits--;
+                
+                // If the count of map happens to be zero
+                if(map.get(fLeft) == 0){
+                    // Pop the left key from the hashmap
                     map.remove(fLeft);
-                }else{
-                    // Decrement the left fruit
-                    map.put(fLeft, map.get(fLeft) - 1);
                 }
-                // Incerement our left pointer
                 left++;
-            }   
-            // Maximize the todal number of fruits you can pick
-            maxFruit = Math.max(maxFruit, right - left + 1);
-            // Incerement our right pointer
-            right++;
+            }
+            
+            maxFruit = Math.max(maxFruit, totalFruits);
         }
         return maxFruit;
     }
