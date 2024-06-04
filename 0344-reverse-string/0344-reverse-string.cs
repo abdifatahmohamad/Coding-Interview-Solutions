5e2128553b9c09b9adc734a84a34b1eddb8abc9e
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2024-06-03 22:39:43 -0500

    Time: 226 ms (52.50%), Space: 129.5 MB (11.58%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public void ReverseString(char[] s) {
+        int left = 0;
+        int right = s.Length - 1;
+        
+        while(left < right){
+            swap(s, left, right);
+            left ++;
+            right --;
+        }
+    }
+    
+    public void swap(char[] s, int i, int j){
+        char temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+    }
+}
