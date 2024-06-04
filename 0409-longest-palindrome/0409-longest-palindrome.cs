26277c2e3554e53d0c53d380dc31f75fd3901b92
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2024-06-03 22:26:37 -0500

    Time: 43 ms (87.10%), Space: 38.5 MB (33.00%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cs
@@ -0,0 +1,32 @@
+public class Solution {
+    public int LongestPalindrome(string s) {
+        if(s.Length == 1){
+            return 1;
+        }
+        
+        Dictionary<char, int> map = new();
+        foreach (char ch in s){
+            map[ch] = map.GetValueOrDefault(ch, 0) + 1;
+        }
+        
+        int res = 0;
+        bool hasOdd = false;
+        foreach (var kvp in map){
+            char key = kvp.Key;
+            int value =  kvp.Value;
+            if(value % 2 == 0){
+                res += value;
+            } else{
+                res += value - 1;
+                hasOdd = true;
+            }
+        }
+        
+        if(hasOdd){
+            res++;
+        }
+        
+        return res;
+        
+    }
+}
