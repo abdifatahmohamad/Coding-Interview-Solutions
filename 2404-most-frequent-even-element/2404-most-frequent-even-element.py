17e22b3d03e887f72ab6aff67859f0f8168cffea
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2024-06-14 00:26:42 -0500

    Time: 235 ms (33.63%), Space: 16.8 MB (96.69%) - LeetHub

--- 2404-most-frequent-even-element/2404-most-frequent-even-element.py
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.py
@@ -5,14 +5,19 @@ class Solution:
             if n % 2 == 0:
                 mapping[n] = mapping.get(n, 0) + 1
         
-        if not mapping:
-            return -1 
+        # if not mapping:
+        #     return -1 
         
-        max_freq = max(mapping.values())
+        max_freq = float("-Inf")
+        for v in mapping.values():
+            max_freq = max(max_freq, v)
+        
+        # max_freq = max(mapping.values())
         res = float("Inf")
         for k, v in mapping.items():
             if v == max_freq:
                 res = min(res, k)
                
-        return res
+        # return res
+        return -1 if res == float("inf") else res
         
